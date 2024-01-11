52dd54ef2333b5bcacb5f206c2979249d6b9cbd8
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-01-11 17:07:34 -0300

    feat: implement custom font loading and splash screen management

--- App.js
+++ App.js
@@ -1,7 +1,35 @@
+import { useFonts } from 'expo-font';
+import * as Splash from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+Splash.preventAutoHideAsync()
+  .then((result) =>
+    console.log(`SplashScreen.preventAutoHideAsync() succeeded: ${result}`)
+  )
+  .catch(console.warn);
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'poppins-bold': require('./src/assets/fonts/Poppins-Bold.ttf'),
+    'poppins-regular': require('./src/assets/fonts/Poppins-Regular.ttf'),
+    'poppins-medium': require('./src/assets/fonts/Poppins-Medium.ttf'),
+  });
+
+  useEffect(() => {
+    async function hideSplashScreen() {
+      await Splash.hideAsync();
+    }
+    if (fontsLoaded) {
+      hideSplashScreen();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
