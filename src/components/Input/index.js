b49722058e7c005d02a1abb1b39b7616d3364163
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-01-12 17:12:01 -0300

    feat: add structure for Input component

---
+++ src/components/Input/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { TextInput, StyleSheet } from 'react-native';
+
+import Colors from '../../constants/Colors';
+
+function Input({ placeholder, maxLength, inputMode, value, onChangeText }) {
+  return (
+    <TextInput
+      style={styles.input}
+      placeholder={placeholder}
+      placeholderTextColor={Colors.text_gray}
+      maxLength={maxLength}
+      inputMode={inputMode}
+      value={value}
+      onChangeText={onChangeText}
+    />
+  );
+}
+
+export default Input;
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: Colors.input_background,
+    width: '100%',
+    height: 56,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: Colors.text_secondary,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    color: Colors.text_gray,
+    fontSize: 16,
+    fontFamily: 'poppins-regular',
+  },
+});
