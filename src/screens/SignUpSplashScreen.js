abaa66da5f6502564824e82cd887d35c921913c1
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-01-12 15:46:16 -0300

    feat: add structure for SignUpSplash screen

---
+++ src/screens/SignUpSplashScreen.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import SignUpSplashScreenImage from '../assets/images/SignUpSplashScreenImage.svg';
+import Button from '../components/Button';
+import Colors from '../constants/Colors';
+
+function SignUpSplashScreen() {
+  return (
+    <View style={styles.SignUpSplash}>
+      <View style={styles.header}>
+        <Text style={styles.title}>REAPP</Text>
+      </View>
+
+      <View style={styles.main}>
+        <View style={styles.illustration}>
+          <SignUpSplashScreenImage width={256} height={274.96} />
+        </View>
+
+        <View style={styles.AccountTypeForm}>
+          <Text style={styles.introduceText}>
+            Obrigado por escolher ser juntar a nós
+          </Text>
+
+          <View style={styles.buttons}>
+            <Button>Sou doador</Button>
+            <Button>Sou Instituição</Button>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default SignUpSplashScreen;
+
+const styles = StyleSheet.create({
+  SignUpSplash: {
+    flex: 1,
+    flexDirection: 'column',
+    marginTop: 30,
+    padding: 16,
+    backgroundColor: Colors.color_white,
+  },
+
+  header: {
+    justifyContent: 'flex-start',
+    flexDirection: 'column',
+  },
+
+  title: {
+    color: Colors.text_primary,
+    fontFamily: 'poppins-bold',
+    fontSize: 24,
+  },
+
+  main: {
+    gap: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 16,
+  },
+
+  AccountTypeForm: {
+    gap: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  introduceText: {
+    fontFamily: 'poppins-bold',
+    fontSize: 16,
+    color: Colors.text_dark,
+  },
+
+  buttons: {
+    gap: 16,
+  },
+});
