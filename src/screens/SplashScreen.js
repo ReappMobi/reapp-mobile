c3a5ebe91533539d48c19fde5f892430b8770eee
andre gabriel <andre.gabriel@discente.ufma.br>
2024-01-12 23:14:47 -0300

    refactor: replace View with ScreenContainer component
    Substituted the use of View with the ScreenContainer component for improved
    structure and functionality.

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -3,11 +3,12 @@ import { View, Text, StyleSheet } from 'react-native';
 
 import SplashScreenImage from '../assets/images/SplashScreenImage.svg';
 import Button from '../components/Button';
+import ScreenContainer from '../components/ScreenContainer';
 import Colors from '../constants/Colors';
 
 function SplashScreen() {
   return (
-    <View style={styles.SplashScreen}>
+    <ScreenContainer>
       <View style={styles.header}>
         <Text style={styles.title}>REAPP</Text>
       </View>
@@ -25,21 +26,13 @@ function SplashScreen() {
 
         <Button>Continuar</Button>
       </View>
-    </View>
+    </ScreenContainer>
   );
 }
 
 export default SplashScreen;
 
 const styles = StyleSheet.create({
-  SplashScreen: {
-    flex: 1,
-    flexDirection: 'column',
-    marginTop: 30,
-    padding: 16,
-    backgroundColor: Colors.color_white,
-  },
-
   header: {
     justifyContent: 'flex-start',
     flexDirection: 'column',
