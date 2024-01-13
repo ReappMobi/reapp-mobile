3b23f10006ca246ef0650efce632845744827ddd
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-01-13 16:07:04 -0300

    refactor: add props on component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,24 +1,20 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
 import React from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, StyleSheet, Pressable } from 'react-native';
 
 import Colors from '../../constants/Colors';
 
-function Button({ children }) {
+function Button({ variant = 'medium', children, backgroundColor }) {
   return (
-    <View style={styles.gridItem}>
+    <View
+      style={[styles.gridItem, variantStyles[variant], { backgroundColor }]}
+    >
       <Pressable
-        style={styles.button}
+        style={({ pressed }) =>
+          pressed ? [styles.button, styles.pressed] : styles.button
+        }
         android_ripple={{ color: Colors.color_gray }}
       >
-        <View style={styles.innerContainer}>
-          <Text style={styles.text}>{children}</Text>
-          <Ionicons
-            name="chevron-forward"
-            size={24}
-            color={Colors.text_neutral}
-          />
-        </View>
+        <View style={styles.innerContainer}>{children}</View>
       </Pressable>
     </View>
   );
@@ -28,9 +24,6 @@ export default Button;
 
 const styles = StyleSheet.create({
   gridItem: {
-    backgroundColor: Colors.color_white,
-    width: 250,
-    height: 56,
     elevation: 4,
     borderRadius: 6,
     shadowColor: '#000',
@@ -55,9 +48,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 
-  text: {
-    fontSize: 16,
-    fontFamily: 'poppins-regular',
-    color: Colors.text_neutral,
+  pressed: {
+    opacity: 0.5,
+  },
+});
+
+const variantStyles = StyleSheet.create({
+  small: {
+    width: 56,
+    height: 56,
+  },
+  medium: {
+    width: 250,
+    height: 56,
+  },
+  large: {
+    width: '100%',
+    height: 56,
   },
 });
