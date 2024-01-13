1f5029f02a133a46bdeb7041930381ab5bde4bfb
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-01-12 21:32:35 -0300

    feat: improving button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,14 +1,25 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 
 import Colors from '../../constants/Colors';
 
 function Button({ children }) {
   return (
-    <View style={styles.button}>
-      <Text style={styles.text}>{children}</Text>
-      <Ionicons name="chevron-forward" size={24} color="black" />
+    <View style={styles.gridItem}>
+      <Pressable
+        style={styles.button}
+        android_ripple={{ color: Colors.color_gray }}
+      >
+        <View style={styles.innerContainer}>
+          <Text style={styles.text}>{children}</Text>
+          <Ionicons
+            name="chevron-forward"
+            size={24}
+            color={Colors.text_neutral}
+          />
+        </View>
+      </Pressable>
     </View>
   );
 }
@@ -16,19 +27,12 @@ function Button({ children }) {
 export default Button;
 
 const styles = StyleSheet.create({
-  button: {
+  gridItem: {
     backgroundColor: Colors.color_white,
     width: 250,
     height: 56,
     elevation: 4,
     borderRadius: 6,
-    borderWidth: 2,
-    borderColor: Colors.color_white,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 16,
-    gap: 10,
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -36,10 +40,24 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
+    overflow: 'hidden',
+  },
+
+  button: {
+    flex: 1,
+  },
+
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
   text: {
     fontSize: 16,
     fontFamily: 'poppins-regular',
+    color: Colors.text_neutral,
   },
 });
