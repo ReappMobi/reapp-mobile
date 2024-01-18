46025b7c94fbe6f2f74c75aab200e489e887c5db
andre gabriel <andre.gabriel@discente.ufma.br>
2024-01-17 22:41:03 -0300

    refactor: move button styles to new file

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Text, StyleSheet, Pressable } from 'react-native';
+import { Text, Pressable } from 'react-native';
 
+import { styles } from './styles';
 import Colors from '../../constants/Colors';
 
 const handlePress = ({ pressed, variant, backgroundColor, size, style }) => {
@@ -36,60 +37,3 @@ export default function Button({
     </Pressable>
   );
 }
-
-const styles = (backgroundColor) =>
-  StyleSheet.create({
-    button: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      padding: 16,
-      borderRadius: 6,
-      backgroundColor,
-    },
-
-    content: {
-      fontFamily: 'poppins-medium',
-    },
-
-    text: {
-      backgroundColor: 'transparent',
-    },
-
-    contained: {
-      elevation: 4,
-      shadowColor: '#000',
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      pressed: {
-        opacity: 0.7,
-      },
-    },
-
-    outlined: {
-      borderWidth: 1,
-      borderColor: backgroundColor,
-      borderRadius: 6,
-      backgroundColor: 'transparent',
-      transition: 'all 0.3s ease-in-out',
-      pressed: {
-        opactiy: 0.5,
-      },
-    },
-
-    sizes: {
-      small: {
-        padding: 8,
-      },
-      medium: {
-        padding: 16,
-      },
-      large: {
-        padding: 24,
-      },
-    },
-  });
