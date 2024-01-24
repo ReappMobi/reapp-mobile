9ec2c69d2ddff4c820f93c96f9079797c64af4ef
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-01-24 17:11:30 -0300

    feat: add ProfileStatistics screen

---
+++ src/screens/ProfileStatisticsScreen/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+
+import DonationInformationItem from '../../components/DonationInformationItem';
+import HeaderStatisticsProfile from '../../components/HeaderStatisticsProfile';
+import ScreenContainer from '../../components/ScreenContainer';
+
+function ProfileStatisticsScreen() {
+  const donations = [
+    {
+      title: 'Doação de Livros',
+      subtitle: '200 livros de literatura para a biblioteca municipal',
+    },
+    {
+      title: 'Equipamentos para Escolas',
+      subtitle: '15 kits de ciências para escolas locais',
+    },
+    {
+      title: 'Apoio a Alimentação',
+      subtitle: '500 kg de alimentos para o banco alimentar da cidade',
+    },
+    {
+      title: 'Roupas para o Inverno',
+      subtitle: '300 casacos e agasalhos para moradores de rua',
+    },
+    {
+      title: 'Patrocínio Esportivo',
+      subtitle: 'Equipamento esportivo para a equipe juvenil de futebol',
+    },
+  ];
+
+  return (
+    <ScreenContainer>
+      <View className="gap-8">
+        <View>
+          <HeaderStatisticsProfile
+            name="Gabriel Bastos"
+            donationsQty={10}
+            followingQty={10}
+            image="https://media.licdn.com/dms/image/D4D03AQEoi_Vob6ydUg/profile-displayphoto-shrink_400_400/0/1679452346440?e=2147483647&v=beta&t=K7GSewyacawx6DLqfhmqcAdkPBkcZgOH1KdAjikRqJA"
+          />
+        </View>
+
+        <Text className="text-center font-_medium text-xl text-text_primary">
+          Minhas doações
+        </Text>
+
+        <View>
+          <FlatList
+            data={donations}
+            renderItem={({ item }) => (
+              <DonationInformationItem
+                title={item.title}
+                subtitle={item.subtitle}
+                image="https://aacd.org.br/wp-content/uploads/2019/10/aacd-doacao.jpg"
+              />
+            )}
+          />
+        </View>
+      </View>
+    </ScreenContainer>
+  );
+}
+
+export default ProfileStatisticsScreen;
