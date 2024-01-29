2599e322855d4f5bd5a2fb5d9e8769963b688f23
andre gabriel <andre.gabriel@discente.ufma.br>
2024-01-29 20:07:43 -0300

    feat: remove extra padding from `ScreenContainer`

--- src/components/ScreenContainer/index.tsx
+++ src/components/ScreenContainer/index.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native';
 
 export default function ScreenContainer({ children }) {
   return (
-    <SafeAreaView className="flex-1 px-4 pt-12">
+    <SafeAreaView className="flex-1 px-4">
       {children}
       <StatusBar style="auto" />
     </SafeAreaView>
