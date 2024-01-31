c1ff8442f4296c47e944b60e22fe1ad49e61c946
andre gabriel <andre.gabriel@discente.ufma.br>
2024-01-31 20:30:08 -0300

    feat(auth): implement authentication context
    Introduce an authentication context to manage user authentication state
    throughout the application, providing a centralized and efficient way
    to handle user login/logout and authentication status.

---
+++ src/contexts/auth.tsx
@@ -0,0 +1,28 @@
+import React, { createContext, useState } from 'react';
+
+import * as auth from '../services/auth';
+
+interface AuthContextData {
+  signed: boolean;
+  user: object | null;
+  signIn(): Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState<object | null>(null);
+
+  async function signIn() {
+    const response = await auth.SignIn();
+    setUser(response.user);
+  }
+
+  return (
+    <AuthContext.Provider value={{ signed: !!user, user, signIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContext;
