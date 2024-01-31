a145805e18c5b63a72ce1499e708fc11ea58536b
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-01-31 19:46:35 -0300

    refactor: change button size on DonationTaxReceiptScreen

--- src/screens/DonationTaxReceiptScreen/index.tsx
+++ src/screens/DonationTaxReceiptScreen/index.tsx
@@ -11,30 +11,32 @@ import Colors from '../../constants/Colors';
 function DonationTaxReceiptScreen() {
   return (
     <ScreenContainer>
-      <Header
-        leftComponent={
-          <Ionicons name="chevron-back" size={24} color={Colors.text_dark} />
-        }
-        rightComponent={
-          <Text className="font-_bold text-2xl uppercase text-text_primary">
-            reapp
-          </Text>
-        }
-      />
+      <View className="py-4">
+        <Header
+          leftComponent={
+            <Ionicons name="chevron-back" size={24} color={Colors.text_dark} />
+          }
+          rightComponent={
+            <Text className="font-_bold text-2xl uppercase text-text_primary">
+              reapp
+            </Text>
+          }
+        />
 
-      <View className="items-center gap-y-4 pt-16">
-        <DonationTaxReceiptImage width={228} height={199} />
+        <View className="items-center gap-y-4 pt-16">
+          <DonationTaxReceiptImage width={228} height={199} />
 
-        <View className="gap-y-2.5">
-          <View>
-            <Button customStyles="w-48 justify-center" textSize="text-sm">
-              Adicionar nota manual
-            </Button>
-          </View>
-          <View>
-            <Button customStyles="w-48 justify-center" textSize="text-sm">
-              Ler QRCode
-            </Button>
+          <View className="gap-y-2.5">
+            <View>
+              <Button customStyles="w-52 justify-center" textSize="text-sm">
+                Adicionar nota manual
+              </Button>
+            </View>
+            <View>
+              <Button customStyles="w-52 justify-center" textSize="text-sm">
+                Ler QRCode
+              </Button>
+            </View>
           </View>
         </View>
       </View>
