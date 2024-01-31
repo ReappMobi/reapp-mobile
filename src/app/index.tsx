37633707b2c4959f32ac451d393a48eadfc5adde
andre gabriel <andre.gabriel@discente.ufma.br>
2024-01-31 20:31:13 -0300

    feat(app): integrate AuthContext in App component
    Introduce AuthContext in the App component to manage user authentication state.
    The App component now conditionally renders the AuthNavigator or placeholder code
    based on the user's authentication status.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,20 @@
 import { useFonts } from 'expo-font';
-import React from 'react';
+import React, { useContext } from 'react';
 
 import { fonts } from '../constants/Fonts';
+import AuthContext from '../contexts/auth';
 import { AuthNavigator } from '../routes/auth.route';
 
 export default function App() {
+  const { signed } = useContext(AuthContext);
   const [fontsLoaded] = useFonts(fonts);
 
   if (!fontsLoaded) {
     return null;
+  }
+
+  if (signed) {
+    // return <AppNavigator />;
   }
 
   return <AuthNavigator />;
