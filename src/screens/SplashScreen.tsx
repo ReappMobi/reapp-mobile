2ed09e601952eebad60e0454f7514abd07d2b956
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-01-31 19:44:14 -0300

    refactor: add vertical padding on SplashScreen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -14,35 +14,37 @@ export default function SplashScreen() {
   const navigation = useNavigation<StackNavigationProp<any>>();
   return (
     <ScreenContainer>
-      <Header
-        leftComponent={
-          <Text className="font-_bold text-2xl uppercase text-text_primary">
-            reapp
-          </Text>
-        }
-      />
-
-      <View className="items-center justify-center gap-y-8">
-        <SplashScreenImage width={256} height={183} />
-        <Text className="font-_medium text-base">
-          Somos uma organização que busca facilitar o encontro de doadores e
-          parceiros com instituições beneficentes. Vamos começar?
-        </Text>
-        <Button
-          customStyles="justify-center gap-x-1 mt-8"
-          endIcon={
-            <Ionicons
-              name="chevron-forward"
-              size={20}
-              color={Colors.text_primary}
-            />
+      <View className="py-4">
+        <Header
+          leftComponent={
+            <Text className="font-_bold text-2xl uppercase text-text_primary">
+              reapp
+            </Text>
           }
-          onPress={() => {
-            navigation.dispatch(StackActions.push('SignupSplash'));
-          }}
-        >
-          Continuar
-        </Button>
+        />
+
+        <View className="items-center justify-center gap-y-8">
+          <SplashScreenImage width={256} height={183} />
+          <Text className="font-_medium text-base">
+            Somos uma organização que busca facilitar o encontro de doadores e
+            parceiros com instituições beneficentes. Vamos começar?
+          </Text>
+          <Button
+            customStyles="justify-center gap-x-1 mt-8"
+            endIcon={
+              <Ionicons
+                name="chevron-forward"
+                size={20}
+                color={Colors.text_primary}
+              />
+            }
+            onPress={() => {
+              navigation.dispatch(StackActions.push('SignupSplash'));
+            }}
+          >
+            Continuar
+          </Button>
+        </View>
       </View>
     </ScreenContainer>
   );
