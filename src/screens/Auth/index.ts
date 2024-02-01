48214ee3155cc1798114e600fd17b039e155a2d3
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-01 20:27:00 -0300

    feat: add new screens to exports

--- src/screens/Auth/index.ts
+++ src/screens/Auth/index.ts
@@ -1,5 +1,15 @@
+import ForgetPasswordResetScreen from './ForgetPasswordResetScreen';
+import ForgetPasswordScreen from './ForgetPasswordScreen';
+import LoginPageScreen from './LoginPageScreen';
 import ProfileSelectorScreen from './ProfileSelectorScreen';
 import SignUpPageScreen from './SignUpPageScreen';
 import WelcomeScreen from './WelcomeScreen';
 
-export { ProfileSelectorScreen, SignUpPageScreen, WelcomeScreen };
+export {
+  ProfileSelectorScreen,
+  SignUpPageScreen,
+  WelcomeScreen,
+  LoginPageScreen,
+  ForgetPasswordResetScreen,
+  ForgetPasswordScreen,
+};
