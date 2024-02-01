42f9ad4bba049df03cd5d9d0b4f91394b251057e
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-01 01:11:04 -0300

    feat: add loginPageScreen

---
+++ src/screens/Auth/LoginPageScreen/index.tsx
@@ -0,0 +1,87 @@
+import { Ionicons } from '@expo/vector-icons';
+import React from 'react';
+import { View, ScrollView, Text } from 'react-native';
+
+import Button from '../../../components/Button';
+import Header from '../../../components/Header';
+import Input from '../../../components/Input';
+import ScreenContainer from '../../../components/ScreenContainer';
+import Colors from '../../../constants/Colors';
+
+function LoginPageScreen() {
+  return (
+    <ScrollView>
+      <ScreenContainer>
+        <View className="py-4">
+          <Header
+            leftComponent={
+              <Text className="font-_bold text-2xl uppercase text-text_primary">
+                reapp
+              </Text>
+            }
+          />
+
+          <View className="gap-3">
+            <View className="flex-row justify-center gap-2">
+              <View>
+                <Button customStyles="w-14 justify-center bg-color_third_light">
+                  <Ionicons
+                    name="logo-facebook"
+                    size={24}
+                    color={Colors.text_neutral}
+                  />
+                </Button>
+              </View>
+
+              <View>
+                <Button customStyles="w-14 justify-center bg-color_third_light">
+                  <Ionicons
+                    name="logo-google"
+                    size={24}
+                    color={Colors.text_neutral}
+                  />
+                </Button>
+              </View>
+            </View>
+
+            <Text className="text-center font-_regular text-xs">
+              Ou cadastre-se com seu email
+            </Text>
+
+            <View>
+              <Text className="font-_regular text-base">Email</Text>
+              <Input placeholder="Digite seu email" inputMode="email" />
+            </View>
+
+            <View>
+              <Text className="font-_regular text-base">Senha</Text>
+              <Input placeholder="Digite sua senha" secureTextEntry />
+            </View>
+
+            <Text className="text-base text-text_primary underline underline-offset-1">
+              Esqueci minha senha
+            </Text>
+
+            <View>
+              <Button
+                customStyles="bg-color_primary w-full justify-center"
+                textColor="text-text_light"
+              >
+                Entrar
+              </Button>
+            </View>
+
+            <Text className="text-center font-_regular text-base">
+              Não possui conta?{' '}
+              <Text className="text-base text-text_primary underline underline-offset-1">
+                Cadastre-se
+              </Text>
+            </Text>
+          </View>
+        </View>
+      </ScreenContainer>
+    </ScrollView>
+  );
+}
+
+export default LoginPageScreen;
