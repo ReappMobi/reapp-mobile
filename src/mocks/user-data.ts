af0c67c6dbbe4842ad2c9cc50b0b2871684de5e6
andre <andre.gabriel@discente.ufma.br>
2024-02-01 22:25:46 -0300

    feat(mock): add user-data mock

---
+++ src/mocks/user-data.ts
@@ -0,0 +1,120 @@
+export interface IUser {
+  id: number;
+  name: string;
+  email: string;
+  following: number;
+  donations: number;
+  profileImage?: string;
+}
+
+export const user: IUser = {
+  id: 1,
+  name: 'Reapp Admin',
+  email: 'reapp_admin@reapp.com',
+  following: 4,
+  donations: 4,
+  profileImage:
+    'https://media.licdn.com/dms/image/D4D03AQEoi_Vob6ydUg/profile-displayphoto-shrink_400_400/0/1679452346440?e=2147483647&v=beta&t=K7GSewyacawx6DLqfhmqcAdkPBkcZgOH1KdAjikRqJA',
+};
+
+export interface IUserDonation {
+  title: string;
+  subtitle?: string;
+}
+
+export const userDonations: IUserDonation[] = [
+  {
+    title: 'Doação de Livros',
+    subtitle: '200 livros de literatura para a biblioteca municipal',
+  },
+  {
+    title: 'Equipamentos para Escolas',
+    subtitle: '15 kits de ciências para escolas locais',
+  },
+  {
+    title: 'Apoio a Alimentação',
+    subtitle: '500 kg de alimentos para o banco alimentar da cidade',
+  },
+  {
+    title: 'Roupas para o Inverno',
+    subtitle: '300 casacos e agasalhos para moradores de rua',
+  },
+  {
+    title: 'Patrocínio Esportivo',
+    subtitle: 'Equipamento esportivo para a equipe juvenil de futebol',
+  },
+  {
+    title: 'Equipamentos para Escolas',
+    subtitle: '15 kits de ciências para escolas locais',
+  },
+  {
+    title: 'Apoio a Alimentação',
+    subtitle: '500 kg de alimentos para o banco alimentar da cidade',
+  },
+  {
+    title: 'Roupas para o Inverno',
+    subtitle: '300 casacos e agasalhos para moradores de rua',
+  },
+  {
+    title: 'Patrocínio Esportivo',
+    subtitle: 'Equipamento esportivo para a equipe juvenil de futebol',
+  },
+];
+
+export interface IUserSavedPost {
+  userImageUrl: string;
+  postImageUrl: string;
+  nameInstitution: string;
+  description: string;
+  timeAgo: string;
+}
+
+export const userSavedPosts: IUserSavedPost[] = [
+  {
+    userImageUrl:
+      'https://blog.emania.com.br/wp-content/uploads/2015/10/fotos-de-natureza.jpg',
+    postImageUrl:
+      'https://blog.emania.com.br/wp-content/uploads/2015/10/fotos-de-natureza.jpg',
+    nameInstitution: 'Universidade ABC',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    timeAgo: '2 hours ago',
+  },
+  {
+    userImageUrl:
+      'https://blog.emania.com.br/wp-content/uploads/2015/10/fotos-de-natureza.jpg',
+    postImageUrl:
+      'https://blog.emania.com.br/wp-content/uploads/2015/10/fotos-de-natureza.jpg',
+    nameInstitution: 'Empresa XYZ',
+    description: 'Sed euismod ligula in nunc bibendum, a eleifend quam congue.',
+    timeAgo: '1 day ago',
+  },
+  {
+    userImageUrl:
+      'https://blog.emania.com.br/wp-content/uploads/2015/10/fotos-de-natureza.jpg',
+    postImageUrl:
+      'https://blog.emania.com.br/wp-content/uploads/2015/10/fotos-de-natureza.jpg',
+    nameInstitution: 'Hospital ABC',
+    description:
+      'Aenean mattis odio sit amet purus gravida, sed vehicula arcu laoreet.',
+    timeAgo: '3 days ago',
+  },
+  {
+    userImageUrl:
+      'https://blog.emania.com.br/wp-content/uploads/2015/10/fotos-de-natureza.jpg',
+    postImageUrl:
+      'https://blog.emania.com.br/wp-content/uploads/2015/10/fotos-de-natureza.jpg',
+    nameInstitution: 'Escola XYZ',
+    description: 'Vestibulum at justo nec ex hendrerit congue.',
+    timeAgo: '4 days ago',
+  },
+  {
+    userImageUrl:
+      'https://blog.emania.com.br/wp-content/uploads/2015/10/fotos-de-natureza.jpg',
+    postImageUrl:
+      'https://blog.emania.com.br/wp-content/uploads/2015/10/fotos-de-natureza.jpg',
+    nameInstitution: 'Companhia ABC',
+    description: 'Phasellus nec libero a lorem ullamcorper dictum.',
+    timeAgo: '5 days ago',
+  },
+];
