75040f71836540b2faa7430b438a5cfeeba31a19
andre <andre.gabriel@discente.ufma.br>
2024-02-01 21:05:19 -0300

    refactor: group all components in a single file

---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+// import cada um dos seguintes componenetes
+// src/components/Button src/components/CardInstitutionProject src/components/CardPost src/components/DonationInformationItem src/components/Header src/components/HeaderStatisticsProfile src/components/Input src/components/ScreenContainer
+import Button from './Button';
+import CardInstitutionProject from './CardInstitutionProject';
+import CardPost from './CardPost';
+import DonationInformationItem from './DonationInformationItem';
+import Header from './Header';
+import HeaderStatisticsProfile from './HeaderStatisticsProfile';
+import Input from './Input';
+import ScreenContainer from './ScreenContainer';
+
+export {
+  Button,
+  CardInstitutionProject,
+  CardPost,
+  DonationInformationItem,
+  Header,
+  HeaderStatisticsProfile,
+  Input,
+  ScreenContainer,
+};
