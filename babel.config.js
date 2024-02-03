4a900ca71b0cb42773bd2e91f2176c713e9e255b
andre <andre.gabriel@discente.ufma.br>
2024-02-02 21:54:28 -0300

    chore(config): add module resolver for absolute paths
    Integrate module resolver in babel.config.js to support absolute paths.

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,26 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel', 'expo-router/babel'],
+    plugins: [
+      'nativewind/babel',
+      'expo-router/babel',
+      [
+        'module-resolver',
+        {
+          root: '.',
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+          ],
+          alias: {},
+        },
+      ],
+    ],
   };
 };
