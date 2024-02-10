664ef15c7e8b5aca370a7f3c766781a8b53d4f38
andre <andre.gabriel@discente.ufma.br>
2024-02-10 19:01:25 -0300

    feat(HomeScreen): integrate post data rendering
    Integrated the rendering of post data into the `HomeScreen` component. The `getPosts` function from
    `app-core.ts` is used to fetch posts, and the retrieved posts are displayed using the `CardPost`
    component within a `FlatList`. Added placeholder text for loading state and fixed a comment related
    to banners scrolling.

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,21 +1,25 @@
 import { useContext, useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import {
   BannersContainer,
   Button,
+  CardPost,
   HeaderStatisticsProfile,
   ScreenContainer,
 } from 'src/components';
 import AuthContext from 'src/contexts/auth';
-import { getSharedCampaigns } from 'src/services/app-core';
+import { getPosts, getSharedCampaigns } from 'src/services/app-core';
 
 export default function HomeScreen() {
   const [banners, setBanners] = useState([]);
+  const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     (async () => {
       const banners = await getSharedCampaigns();
       setBanners(banners);
+      const posts = await getPosts();
+      setPosts(posts);
     })();
   }, []);
 
@@ -30,14 +34,33 @@ export default function HomeScreen() {
             donationsQty={user.donations}
             followingQty={user.following}
           />
+          {/*FIXME: Banners don't scroll with the screen */}
           <Button customStyles="bg-color_third py-2 px-4 my-4 justify-center w-full">
             <Text className="self-center text-center font-_medium text-lg text-white">
               Doar para instituições sociais
             </Text>
           </Button>
           <View className="gap-y-2">
+            {/* TODO: Post should be loaded partially */}
             <BannersContainer banners={banners} />
-            <Text>List Posts</Text>
+            {posts.length === 0 ? (
+              <Text>Carregando...</Text>
+            ) : (
+              <FlatList
+                data={posts}
+                renderItem={({ item }) => (
+                  <CardPost
+                    imagePath={item.postImageUrl}
+                    userImagePath={item.userImageUrl}
+                    nameInstitution={item.nameInstitution}
+                    description={item.description}
+                    timeAgo={item.timeAgo}
+                    isSavedInitial={false}
+                  />
+                )}
+                ItemSeparatorComponent={() => <View className="mb-2.5" />}
+              />
+            )}
           </View>
         </View>
       </ScreenContainer>
