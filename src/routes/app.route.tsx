b307591702d9bd83216caecf4e412f43677cb010
andre <andre.gabriel@discente.ufma.br>
2024-02-10 16:15:41 -0300

    feat(route): add initial implementation of AppNavigator component

---
+++ src/routes/app.route.tsx
@@ -0,0 +1,14 @@
+import { Tabs } from 'expo-router/tabs';
+
+export function AppNavigator() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          href: null,
+        }}
+      />
+    </Tabs>
+  );
+}
