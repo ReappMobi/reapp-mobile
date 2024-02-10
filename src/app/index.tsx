cd0d69e74359a55a5a41474aab6ef5fef8186640
andre <andre.gabriel@discente.ufma.br>
2024-02-10 11:04:16 -0300

    refactor: rename import paths to import from `src/`

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,8 @@
 import { useFonts } from 'expo-font';
 import React, { useContext } from 'react';
-
-import { fonts } from '../constants/Fonts';
-import AuthContext from '../contexts/auth';
-import { AuthNavigator } from '../routes/auth.route';
+import { fonts } from 'src/constants/Fonts';
+import AuthContext from 'src/contexts/auth';
+import { AuthNavigator } from 'src/routes/auth.route';
 
 export default function App() {
   const { signed } = useContext(AuthContext);
