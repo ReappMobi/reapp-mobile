e18f978c59d271cb8d35b22ed1d64713669f31b3
andre <andre.gabriel@discente.ufma.br>
2024-02-10 18:34:45 -0300

    style(banner): adjust Banner component to use full height and width for the image

--- src/components/BannersContainer/Banner/index.tsx
+++ src/components/BannersContainer/Banner/index.tsx
@@ -1,10 +1,9 @@
-import { View, Image, Text } from 'react-native';
+import { View, Image } from 'react-native';
 
 export function Banner({ image, title }) {
   return (
-    <View className="mr-1 w-screen bg-slate-600">
-      <Image aria-label={title} source={{ uri: image }} />
-      <Text>{image}</Text>
+    <View className="mr-1 h-56 w-screen bg-slate-600">
+      <Image className="h-full w-full" source={{ uri: image }} />
     </View>
   );
 }
