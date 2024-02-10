6ef046f02bd3e86305af9625fa858c96b22e46d8
andre <andre.gabriel@discente.ufma.br>
2024-02-10 18:26:37 -0300

    feat(route): update AppNavigator to use createBottomTabNavigator and include HomeScreen component

--- src/routes/app.route.tsx
+++ src/routes/app.route.tsx
@@ -1,14 +1,24 @@
-import { Tabs } from 'expo-router/tabs';
+import { Octicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Colors from 'src/constants/Colors';
+import HomeScreen from 'src/screens/HomeScreen';
 
 export function AppNavigator() {
+  const Tab = createBottomTabNavigator();
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="index"
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="home"
+        component={HomeScreen}
         options={{
-          href: null,
+          tabBarActiveTintColor: Colors.color_primary,
+          tabBarInactiveTintColor: Colors.text_neutral,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Octicons name="home" size={size} color={color} />
+          ),
         }}
       />
-    </Tabs>
+    </Tab.Navigator>
   );
 }
