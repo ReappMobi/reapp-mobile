c479c832edde126d52b44610033c68d87b9677de
andre <andre.gabriel@discente.ufma.br>
2024-02-10 18:57:48 -0300

    feat(Banner): add aria-label to Banner component
    Added aria-label attribute to the Image component within the Banner component to improve
    accessibility.

--- src/components/BannersContainer/Banner/index.tsx
+++ src/components/BannersContainer/Banner/index.tsx
@@ -3,7 +3,11 @@ import { View, Image } from 'react-native';
 export function Banner({ image, title }) {
   return (
     <View className="mr-1 h-56 w-screen bg-slate-600">
-      <Image className="h-full w-full" source={{ uri: image }} />
+      <Image
+        aria-label={title}
+        className="h-full w-full"
+        source={{ uri: image }}
+      />
     </View>
   );
 }
