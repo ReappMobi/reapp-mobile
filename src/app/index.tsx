8ab5815df52e3bfc43f0ccb5916060072aeedaad
andre <andre.gabriel@discente.ufma.br>
2024-02-10 16:18:24 -0300

    refactor(app): refactor App component to use AuthProvider and Routes for navigation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,18 @@
 import { useFonts } from 'expo-font';
-import React, { useContext } from 'react';
 import { fonts } from 'src/constants/Fonts';
-import AuthContext from 'src/contexts/auth';
-import { AuthNavigator } from 'src/routes/auth.route';
+import { AuthProvider } from 'src/contexts/auth';
+import Routes from 'src/routes';
 
 export default function App() {
-  const { signed } = useContext(AuthContext);
   const [fontsLoaded] = useFonts(fonts);
 
   if (!fontsLoaded) {
     return null;
   }
 
-  if (signed) {
-    // return <AppNavigator />;
-  }
-
-  return <AuthNavigator />;
+  return (
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
+  );
 }
