cbf172036405b7600fec00b3cbf56829236b9184
andre <andre.gabriel@discente.ufma.br>
2024-02-10 18:41:15 -0300

    chore(mock): update app banners mock data to use PNG images with specific URLs

--- src/mocks/app-banners-data.ts
+++ src/mocks/app-banners-data.ts
@@ -8,16 +8,16 @@ export const banners: IBanner[] = [
   {
     id: 0,
     title: 'campanha1',
-    image: 'https://placehold.co/600x400',
+    image: 'https://placehold.co/600x400.png',
   },
   {
     id: 1,
     title: 'campanha2',
-    image: 'https://placehold.co/600x400',
+    image: 'https://placehold.co/600x400.png',
   },
   {
     id: 2,
     title: 'campanha3',
-    image: 'https://placehold.co/600x400',
+    image: 'https://placehold.co/600x400.png',
   },
 ];
