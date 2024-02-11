c5fa6caee7b4e2423fd9bef0b0cc35f576914db9
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-11 19:18:27 -0300

    feat: add projects  mock data

---
+++ src/mocks/app-projects-data.ts
@@ -0,0 +1,211 @@
+export interface IProject {
+  id: number;
+  nameProject: string;
+  idInstitution: number;
+  idCategory: number;
+  description: string;
+  imageUrl: string;
+}
+
+export const projects: IProject[] = [
+  {
+    id: 1,
+    nameProject: 'Reforestation_1',
+    idInstitution: 16,
+    idCategory: 1,
+    description:
+      'Efforts to replant and restore lost forest areas, combating climate change and preserving biodiversity. Project number 1 focuses on reforestation.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 2,
+    nameProject: 'HomelessSupport_2',
+    idInstitution: 9,
+    idCategory: 1,
+    description:
+      'Aid and resources for homeless individuals, aiming to provide shelter, healthcare, and employment opportunities. Project number 2 focuses on homeless support.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 3,
+    nameProject: 'AnimalRescue_3',
+    idInstitution: 3,
+    idCategory: 1,
+    description:
+      'Rescue and rehabilitation of abandoned or injured animals, providing them with care and a safe habitat. Project number 3 focuses on animal rescue.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 4,
+    nameProject: 'HomelessSupport_4',
+    idInstitution: 19,
+    idCategory: 1,
+    description:
+      'Aid and resources for homeless individuals, aiming to provide shelter, healthcare, and employment opportunities. Project number 4 focuses on homeless support.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 5,
+    nameProject: 'OtherInitiatives_5',
+    idInstitution: 15,
+    idCategory: 4,
+    description:
+      'Projects focused on various other initiatives that do not relate to homelessness, reforestation, or animal rescue. Project number 5 focuses on other initiatives.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 6,
+    nameProject: 'AnimalRescue_6',
+    idInstitution: 18,
+    idCategory: 1,
+    description:
+      'Rescue and rehabilitation of abandoned or injured animals, providing them with care and a safe habitat. Project number 6 focuses on animal rescue.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 7,
+    nameProject: 'AnimalRescue_7',
+    idInstitution: 8,
+    idCategory: 3,
+    description:
+      'Rescue and rehabilitation of abandoned or injured animals, providing them with care and a safe habitat. Project number 7 focuses on animal rescue.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 8,
+    nameProject: 'Reforestation_8',
+    idInstitution: 15,
+    idCategory: 2,
+    description:
+      'Efforts to replant and restore lost forest areas, combating climate change and preserving biodiversity. Project number 8 focuses on reforestation.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 9,
+    nameProject: 'AnimalRescue_9',
+    idInstitution: 5,
+    idCategory: 3,
+    description:
+      'Rescue and rehabilitation of abandoned or injured animals, providing them with care and a safe habitat. Project number 9 focuses on animal rescue.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 10,
+    nameProject: 'Reforestation_10',
+    idInstitution: 13,
+    idCategory: 2,
+    description:
+      'Efforts to replant and restore lost forest areas, combating climate change and preserving biodiversity. Project number 10 focuses on reforestation.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 11,
+    nameProject: 'AnimalRescue_11',
+    idInstitution: 7,
+    idCategory: 1,
+    description:
+      'Rescue and rehabilitation of abandoned or injured animals, providing them with care and a safe habitat. Project number 11 focuses on animal rescue.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 12,
+    nameProject: 'HomelessSupport_12',
+    idInstitution: 13,
+    idCategory: 2,
+    description:
+      'Aid and resources for homeless individuals, aiming to provide shelter, healthcare, and employment opportunities. Project number 12 focuses on homeless support.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 13,
+    nameProject: 'Reforestation_13',
+    idInstitution: 3,
+    idCategory: 3,
+    description:
+      'Efforts to replant and restore lost forest areas, combating climate change and preserving biodiversity. Project number 13 focuses on reforestation.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 14,
+    nameProject: 'OtherInitiatives_14',
+    idInstitution: 4,
+    idCategory: 4,
+    description:
+      'Projects focused on various other initiatives that do not relate to homelessness, reforestation, or animal rescue. Project number 14 focuses on other initiatives.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 15,
+    nameProject: 'HomelessSupport_15',
+    idInstitution: 9,
+    idCategory: 2,
+    description:
+      'Aid and resources for homeless individuals, aiming to provide shelter, healthcare, and employment opportunities. Project number 15 focuses on homeless support.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 16,
+    nameProject: 'HomelessSupport_16',
+    idInstitution: 2,
+    idCategory: 2,
+    description:
+      'Aid and resources for homeless individuals, aiming to provide shelter, healthcare, and employment opportunities. Project number 16 focuses on homeless support.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 17,
+    nameProject: 'HomelessSupport_17',
+    idInstitution: 1,
+    idCategory: 1,
+    description:
+      'Aid and resources for homeless individuals, aiming to provide shelter, healthcare, and employment opportunities. Project number 17 focuses on homeless support.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 18,
+    nameProject: 'HomelessSupport_18',
+    idInstitution: 16,
+    idCategory: 2,
+    description:
+      'Aid and resources for homeless individuals, aiming to provide shelter, healthcare, and employment opportunities. Project number 18 focuses on homeless support.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 19,
+    nameProject: 'OtherInitiatives_19',
+    idInstitution: 3,
+    idCategory: 4,
+    description:
+      'Projects focused on various other initiatives that do not relate to homelessness, reforestation, or animal rescue. Project number 19 focuses on other initiatives.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+  {
+    id: 20,
+    nameProject: 'Reforestation_20',
+    idInstitution: 18,
+    idCategory: 3,
+    description:
+      'Efforts to replant and restore lost forest areas, combating climate change and preserving biodiversity. Project number 20 focuses on reforestation.',
+    imageUrl:
+      'https://ampliar.org.br/wp-content/uploads/2021/02/de-quais-formas-eu-posso-fazer-doacao-para-um-projeto-social-01.jpg',
+  },
+];
