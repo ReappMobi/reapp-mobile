93642a0b53b9df4c46b0483e4af2720e0272162c
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-10 22:02:58 -0300

    chore(components): update components index to include new components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,11 +1,14 @@
 import Button from './Button';
 import CardInstitutionProject from './CardInstitutionProject';
 import CardPost from './CardPost';
+import CardSearch from './CardSearch';
 import DonationInformationItem from './DonationInformationItem';
+import ExploreScreenCard from './ExploreScreenCard';
 import Header from './Header';
 import HeaderStatisticsProfile from './HeaderStatisticsProfile';
 import Input from './Input';
 import ScreenContainer from './ScreenContainer';
+import SearchInput from './SearchInput';
 
 export {
   Button,
@@ -16,4 +19,7 @@ export {
   HeaderStatisticsProfile,
   Input,
   ScreenContainer,
+  SearchInput,
+  ExploreScreenCard,
+  CardSearch,
 };
