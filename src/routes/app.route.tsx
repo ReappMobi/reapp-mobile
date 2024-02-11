e1d651a46dcc318f2913001cd244462ac51db89c
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-11 19:27:10 -0300

    refactor: implement navigation to Explore screens

--- src/routes/app.route.tsx
+++ src/routes/app.route.tsx
@@ -1,7 +1,26 @@
 import { Octicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import Colors from 'src/constants/Colors';
+import ExploreProjectsScreen from 'src/screens/Explore/ExploreProjectsScreen';
+import ExploreScreen from 'src/screens/Explore/ExploreScreen';
 import HomeScreen from 'src/screens/HomeScreen';
+
+const Explore = () => {
+  const ExploreStack = createStackNavigator();
+  return (
+    <ExploreStack.Navigator screenOptions={{ headerShown: false }}>
+      <ExploreStack.Screen
+        name="ExploreInstitutions"
+        component={ExploreScreen}
+      />
+      <ExploreStack.Screen
+        name="ExploreProjects"
+        component={ExploreProjectsScreen}
+      />
+    </ExploreStack.Navigator>
+  );
+};
 
 export function AppNavigator() {
   const Tab = createBottomTabNavigator();
@@ -16,6 +35,19 @@ export function AppNavigator() {
           tabBarShowLabel: false,
           tabBarIcon: ({ size, color }) => (
             <Octicons name="home" size={size} color={color} />
+          ),
+        }}
+      />
+
+      <Tab.Screen
+        name="Explore"
+        component={Explore}
+        options={{
+          tabBarActiveTintColor: Colors.color_primary,
+          tabBarInactiveTintColor: Colors.text_neutral,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ size, color }) => (
+            <Octicons name="search" size={size} color={color} />
           ),
         }}
       />
