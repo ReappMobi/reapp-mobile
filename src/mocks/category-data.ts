c89d6f9e940b828af1667f3c30e1c53c600b75f1
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-10 22:09:55 -0300

    feat(data): add mock data for category institutions

---
+++ src/mocks/category-data.ts
@@ -0,0 +1,10 @@
+export interface ICategory {
+  category: string;
+  id: number;
+}
+
+export const categories: ICategory[] = [
+  { category: 'Infância', id: 1 },
+  { category: 'Adolescência', id: 2 },
+  { category: 'Adultos', id: 3 },
+];
