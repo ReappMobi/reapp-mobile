d2a8b36b1537750717132856a1348af5c68731b1
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-11 19:32:31 -0300

    feat(service): add function in app-core.ts

--- src/services/app-core.ts
+++ src/services/app-core.ts
@@ -1,5 +1,9 @@
+import { institutionCategories } from 'src/mocks/app-InstitutionCategory-data';
 import { banners } from 'src/mocks/app-banners-data';
+import { institutions } from 'src/mocks/app-institution-data';
 import { allPosts } from 'src/mocks/app-posts-data';
+import { projects } from 'src/mocks/app-projects-data';
+import { projectCategories } from 'src/mocks/app-projectsCategory-data';
 
 export async function getSharedCampaigns() {
   setTimeout(() => {
@@ -13,4 +17,32 @@ export async function getPosts() {
     console.log('fetchin posts...');
   }, 5000);
   return allPosts;
+}
+
+export async function getProjects() {
+  setTimeout(() => {
+    console.log('fetchin projects...');
+  }, 5000);
+  return projects;
+}
+
+export async function getInstitutions() {
+  setTimeout(() => {
+    console.log('fetchin institutions...');
+  }, 5000);
+  return institutions;
+}
+
+export async function getInstituitionCategories() {
+  setTimeout(() => {
+    console.log('fetchin institution categories...');
+  }, 5000);
+  return institutionCategories;
+}
+
+export async function getProjectCategories() {
+  setTimeout(() => {
+    console.log('fetchin project categories...');
+  }, 5000);
+  return projectCategories;
 }
