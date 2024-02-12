f12f81e65264cca13df786d45dece978cb01d6f8
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-12 14:08:59 -0300

    feat(screen): add Transparency screen

---
+++ src/screens/TransparencyScreen/index.tsx
@@ -0,0 +1,94 @@
+import { FontAwesome5 } from '@expo/vector-icons';
+import React, { useEffect, useState } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { ScreenContainer, Header } from 'src/components';
+import {
+  getDonationsByEixo,
+  getDonationsBySegment,
+  getTotalValueDonation,
+} from 'src/services/app-core';
+
+function TransparencyScreen() {
+  const [donationsByEixo, setDonationsByEixo] = useState([]);
+  const [donationsBySegment, setDonationsBySegment] = useState([]);
+  const [totalValueDonations, setTotalValueDonations] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const donationsByEixoData = await getDonationsByEixo();
+      setDonationsByEixo(donationsByEixoData);
+      const donationsBySegmentData = await getDonationsBySegment();
+      setDonationsBySegment(donationsBySegmentData);
+      const totalValueDonationsData = await getTotalValueDonation();
+      setTotalValueDonations(totalValueDonationsData.total);
+    })();
+  }, []);
+
+  return (
+    <ScrollView>
+      <ScreenContainer>
+        <View className="flex-1 py-4">
+          <Header
+            centerComponent={
+              <View className="flex-row items-center gap-x-2">
+                <Text className="font-_bold text-base text-text_neutral">
+                  Transparência
+                </Text>
+              </View>
+            }
+          />
+
+          <View className="mb-9 flex-row items-center justify-between border-b pb-4">
+            <View>
+              <Text className="font-_bold text-base text-text_neutral">
+                Total de doações até o momento
+              </Text>
+              <Text className="font-_medium text-base text-text_neutral">
+                R$ {totalValueDonations}
+              </Text>
+            </View>
+            <View>
+              <FontAwesome5 name="coins" size={27} color="black" />
+            </View>
+          </View>
+
+          <View className="mb-9 border-b">
+            <Text className="mb-4 font-_bold text-base text-text_neutral">
+              Eixos
+            </Text>
+
+            {donationsByEixo.map((item, idx) => (
+              <View key={idx} className="mb-4 flex-row justify-between">
+                <Text className="font-_medium text-base text-text_neutral">
+                  {item.eixo}
+                </Text>
+                <Text className="font-_medium text-base text-text_neutral">
+                  R$ {item.value}
+                </Text>
+              </View>
+            ))}
+          </View>
+
+          <View>
+            <Text className="mb-4 font-_bold text-base text-text_neutral">
+              Por segmento
+            </Text>
+
+            {donationsBySegment.map((item, idx) => (
+              <View key={idx} className="mb-4 flex-row justify-between">
+                <Text className="font-_medium text-base text-text_neutral">
+                  {item.segment}
+                </Text>
+                <Text className="font-_medium text-base text-text_neutral">
+                  R$ {item.value}
+                </Text>
+              </View>
+            ))}
+          </View>
+        </View>
+      </ScreenContainer>
+    </ScrollView>
+  );
+}
+
+export default TransparencyScreen;
