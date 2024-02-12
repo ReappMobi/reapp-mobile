76ad137e4842c374c663f2b43e48c7f869a109d2
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-12 14:06:34 -0300

    feat: add donationsByEixo mock data

---
+++ src/mocks/app-donationEixo-data.ts
@@ -0,0 +1,13 @@
+export interface IDonationEixo {
+  eixo: string;
+  value: string;
+}
+
+export const donationsByEixo: IDonationEixo[] = [
+  { eixo: 'Idoso', value: '31.450.023,45' },
+  { eixo: 'Cultura', value: '11.000.000,00' },
+  { eixo: 'Adulto', value: '3.000.000,00' },
+  { eixo: 'Meio ambiente', value: '2.000.000,00' },
+  { eixo: 'Infância', value: '500.000,00' },
+  { eixo: 'Animais', value: ' 500.000,00' },
+];
