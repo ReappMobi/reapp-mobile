19f04e5ce523d68df2a2895795fb0fac5039e153
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-12 14:07:02 -0300

    feat: add donationsBySegment mock data

---
+++ src/mocks/app-donationSegment-data.ts
@@ -0,0 +1,14 @@
+export interface IDonationSegment {
+  segment: string;
+  value: string;
+}
+
+export const donationsBySegment: IDonationSegment[] = [
+  { segment: 'Alzheimer', value: '31.450.023,45' },
+  { segment: 'Bumba meu boi', value: '11.000.000,00' },
+  { segment: 'Adulto sem teto', value: '3.000.000,00' },
+  { segment: 'Amazônia', value: '2.000.000,00' },
+  { segment: 'Resgate de animais', value: '500.000,00' },
+  { segment: 'Primeira Infância', value: ' 200.000,00' },
+  { segment: 'Segunda Infância', value: ' 100.000,00' },
+];
