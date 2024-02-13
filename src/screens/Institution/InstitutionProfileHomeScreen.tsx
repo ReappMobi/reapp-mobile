f15fdf7635dcf56a4e30bd020e7a0efd0befc635
andre <andre.gabriel@discente.ufma.br>
2024-02-13 12:24:19 -0300

    feat(Institution): add InstitutionProfileHomeScreen component

---
+++ src/screens/Institution/InstitutionProfileHomeScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import { ScreenContainer } from 'src/components';
+
+export default function InstitutionProfileHomeScreen() {
+  return (
+    <ScreenContainer>
+      <View>
+        <Text>InstitutionProfileHomeScreen</Text>
+      </View>
+    </ScreenContainer>
+  );
+}
