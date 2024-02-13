bc3d535ab0f468d2413a8cfd7db36afc341e14db
andre <andre.gabriel@discente.ufma.br>
2024-02-13 15:07:08 -0300

    feat(screen): enhance InstitutionProfileHomeScreen layout
    Improve the layout of InstitutionProfileHomeScreen with added details, buttons, and styling.

--- src/screens/Institution/InstitutionProfileHomeScreen.tsx
+++ src/screens/Institution/InstitutionProfileHomeScreen.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
-import { ScreenContainer } from 'src/components';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image } from 'react-native';
+import { ScreenContainer, Button } from 'src/components';
 import { ICategory } from 'src/mocks/app-InstitutionCategory-data';
 import { getCategoryById } from 'src/services/app-core';
 import { IInstitution } from 'src/types';
@@ -23,23 +24,59 @@ export default function InstitutionProfileHomeScreen({ route }) {
   const PlaceholderLoader = () => {
     return (
       <View className="animate-pulse">
-        <View className="h-2.5 w-24 rounded-full bg-gray-300" />
+        <View className="my-1 h-2.5 w-24 rounded-full bg-gray-300" />
       </View>
     );
   };
 
   return (
-    <ScreenContainer>
-      <View className="pt-4">
-        <Text className="font-_bold text-lg">
-          {institution.nameInstitution}
-        </Text>
-        {loading ? (
-          <PlaceholderLoader />
-        ) : (
-          <Text className="text-sm">{category}</Text>
-        )}
-      </View>
-    </ScreenContainer>
+    <View className="flex-1 bg-white">
+      <ScreenContainer>
+        <View className="flex-row items-center space-x-2">
+          <Image
+            className="h-16 w-16 rounded-full"
+            source={{ uri: institution.imageUrl }}
+          />
+          <View className="w-full flex-1 space-y-0 pt-4">
+            <Text className="font-_bold text-lg">
+              {institution.nameInstitution}
+            </Text>
+            {loading ? (
+              <PlaceholderLoader />
+            ) : (
+              <Text className="text-md mb-1">{category}</Text>
+            )}
+            <View className="space-y-2">
+              <Button
+                textColor="text-white"
+                size="small"
+                customStyles="justify-center bg-color_primary"
+              >
+                Seguir
+              </Button>
+              <View className="flex-row">
+                <Button
+                  textColor="text-white"
+                  size="small"
+                  customStyles="justify-center bg-color_primary w-20 mr-2"
+                >
+                  Doar
+                </Button>
+                <Button
+                  startIcon={
+                    <Ionicons name="chevron-forward" size={20} color="#000" />
+                  }
+                  customStyles="flex-1 justify-start items-center space-x-1"
+                  size="small"
+                  textSize="text-sm"
+                >
+                  Quero ser voluntário
+                </Button>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScreenContainer>
+    </View>
   );
 }
