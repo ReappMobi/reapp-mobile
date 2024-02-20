7a8e0024afb8d03ed06ee34f521b4780d59f7507
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-20 10:45:29 -0300

    feat: add IProject interface

---
+++ src/types/IProject.ts
@@ -0,0 +1,10 @@
+export interface IProject {
+  id: number;
+  nameProject: string;
+  subtitle: string;
+  idInstitution: number;
+  idCategory: number;
+  description: string;
+  imageUrl: string;
+  videoIntroUrl: string;
+}
