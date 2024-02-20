1336a5b6d6112a679553b51cec9ae8553a11da54
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-20 10:38:00 -0300

    refactor: import IProject type from src/types

--- src/mocks/app-projects-data.ts
+++ src/mocks/app-projects-data.ts
@@ -1,13 +1,4 @@
-export interface IProject {
-  id: number;
-  nameProject: string;
-  subtitle: string;
-  idInstitution: number;
-  idCategory: number;
-  description: string;
-  imageUrl: string;
-  videoIntroUrl: string;
-}
+import { IProject } from 'src/types';
 
 export const projects: IProject[] = [
   {
