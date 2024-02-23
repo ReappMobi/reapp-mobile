0540685c00d4903c698461302e0b642867cb463f
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-02-22 22:39:18 -0300

    feat: group app screens in Screen folder

---
+++ src/screens/index.ts
@@ -0,0 +1,29 @@
+import DonationConfirmScreen from './App/Donation/DonationConfirmScreen';
+import DonationMethodScreen from './App/Donation/DonationMethodScreen';
+import DonationScreen from './App/Donation/DonationScreen';
+import DonationTaxReceiptScreen from './App/Donation/DonationTaxReceiptScreen';
+import Explore from './App/Explore';
+import FavoritePage from './App/FavoritePage';
+import HomeScreen from './App/HomeScreen';
+import InstitutionProfile from './App/InstitutionProfile';
+import ProfileSavedScreen from './App/ProfileSavedScreen';
+import ProfileStatisticsScreen from './App/ProfileStatisticsScreen';
+import ProjectPage from './App/ProjectPage';
+import TransparencyScreen from './App/TransparencyScreen';
+import UserProfilePage from './App/UserProfilePage';
+
+export {
+  DonationConfirmScreen,
+  DonationMethodScreen,
+  DonationScreen,
+  DonationTaxReceiptScreen,
+  Explore,
+  FavoritePage,
+  HomeScreen,
+  InstitutionProfile,
+  ProfileSavedScreen,
+  ProfileStatisticsScreen,
+  ProjectPage,
+  TransparencyScreen,
+  UserProfilePage,
+};
