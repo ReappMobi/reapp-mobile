fbcc03fbe7807d354bb19f2a7d213f9e93566f1d
andre <andre.gabriel@discente.ufma.br>
2024-02-23 00:00:20 -0300

    feat: update function definitions
    - Add function types.
    - Fix function delay to be more realistic.

--- src/services/app-core.ts
+++ src/services/app-core.ts
@@ -3,69 +3,92 @@ import {
   institutionCategories,
 } from 'src/mocks/app-InstitutionCategory-data';
 import { banners } from 'src/mocks/app-banners-data';
-import { donationsByEixo } from 'src/mocks/app-donationEixo-data';
-import { donationsBySegment } from 'src/mocks/app-donationSegment-data';
+import {
+  IDonationEixo,
+  donationsByEixo,
+} from 'src/mocks/app-donationEixo-data';
+import {
+  IDonationSegment,
+  donationsBySegment,
+} from 'src/mocks/app-donationSegment-data';
 import { institutions } from 'src/mocks/app-institution-data';
 import { posts } from 'src/mocks/app-institution-post-data';
 import { allPosts } from 'src/mocks/app-posts-data';
 import { projects } from 'src/mocks/app-projects-data';
 import { projectCategories } from 'src/mocks/app-projectsCategory-data';
-import { IPost, IProject } from 'src/types';
+import { IBanner, IInstitution, IPost, IProject } from 'src/types';
 
-export async function getSharedCampaigns() {
-  setTimeout(() => {
-    console.log('fetchin banners...');
-  }, 2000);
-  return banners;
+export async function getSharedCampaigns(): Promise<IBanner[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('fetching banners...');
+      resolve(banners);
+    }, 1000);
+  });
 }
 
-export async function getPosts() {
-  setTimeout(() => {
-    console.log('fetchin posts...');
-  }, 5000);
-  return allPosts;
+/**TODO: fix Post type in this function */
+export async function getPosts(): Promise<any> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('fetching posts...');
+      resolve(allPosts);
+    }, 1000);
+  });
 }
 
-export async function getProjects() {
-  setTimeout(() => {
-    console.log('fetchin projects...');
-  }, 5000);
-  return projects;
+export async function getProjects(): Promise<IProject[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('fetching projects...');
+      resolve(projects);
+    }, 1000);
+  });
 }
 
-export async function getInstitutions() {
-  setTimeout(() => {
-    console.log('fetchin institutions...');
-  }, 5000);
-  return institutions;
+export async function getInstitutions(): Promise<IInstitution[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('fetching institutions...');
+      resolve(institutions);
+    }, 1000);
+  });
 }
 
-export async function getInstituitionCategories() {
-  setTimeout(() => {
-    console.log('fetchin institution categories...');
-  }, 5000);
-  return institutionCategories;
+export async function getInstituitionCategories(): Promise<ICategory[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('fetching institutions...');
+      resolve(institutionCategories);
+    }, 1000);
+  });
 }
 
-export async function getProjectCategories() {
-  setTimeout(() => {
-    console.log('fetchin project categories...');
-  }, 5000);
-  return projectCategories;
+export async function getProjectCategories(): Promise<ICategory[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('fetchin project categories...');
+      resolve(projectCategories);
+    }, 1000);
+  });
 }
 
-export async function getDonationsByEixo() {
-  setTimeout(() => {
-    console.log('fetchin donations by eixo...');
-  }, 5000);
-  return donationsByEixo;
+export async function getDonationsByEixo(): Promise<IDonationEixo[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('fetchin donations by field...');
+      resolve(donationsByEixo);
+    }, 1000);
+  });
 }
 
-export async function getDonationsBySegment() {
-  setTimeout(() => {
-    console.log('fetchin donations by segment...');
-  }, 5000);
-  return donationsBySegment;
+export async function getDonationsBySegment(): Promise<IDonationSegment[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('fetchin donations by segment...');
+      resolve(donationsBySegment);
+    }, 5000);
+  });
 }
 
 export async function getCategoryById(id: number): Promise<ICategory> {
