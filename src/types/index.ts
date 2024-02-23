bd76b596cbfc4de487f72694bc3f84c8c040742d
andre <andre.gabriel@discente.ufma.br>
2024-02-22 22:30:20 -0300

    feat: add IBanner type definition

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,4 @@ import { IInstitution } from './IInstitution';
 export type { IInstitution };
 export type { IPost } from './IPost';
 export type { IProject } from './IProject';
+export type { IBanner } from './IBanner';
