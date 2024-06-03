95bfc7a77e8cc6e6df01721f68e2329132f6a301
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-06-03 00:07:55 -0300

    refactor: change route in payment service

--- src/services/payment.ts
+++ src/services/payment.ts
@@ -2,7 +2,7 @@ import api from './api';
 
 export const requestPaymentUrl = async (data) => {
   try {
-    const response = await api.post('/payment/request/institution', data, {
+    const response = await api.post('/payment/request', data, {
       headers: {
         'Content-Type': 'application/json',
       },
