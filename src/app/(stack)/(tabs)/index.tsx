2646fc75623f4e764a4ba49071434c01638bb33e
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-07-23 22:47:04 -0300

    refactor: update tab index screen

--- src/app/(stack)/(tabs)/index.tsx
+++ src/app/(stack)/(tabs)/index.tsx
@@ -1,19 +1,24 @@
+import { parseISO } from 'date-fns';
 import { router } from 'expo-router';
 import { useCallback, useEffect, useState } from 'react';
 import { View, FlatList, ListRenderItem } from 'react-native';
 import { Button, CardPost, Carousel } from 'src/components';
-import { IPost } from 'src/mocks/app-posts-data';
+import { useAuth } from 'src/hooks/useAuth';
 import { getPosts, getSharedCampaigns } from 'src/services/app-core';
+import { IPost } from 'src/types';
 
 export default function Page() {
   const [banners, setBanners] = useState([]);
   const [posts, setPosts] = useState([]);
+  const { isDonor } = useAuth();
+  const auth = useAuth();
 
   useEffect(() => {
     (async () => {
+      const token = await auth.getToken();
       const banners = await getSharedCampaigns();
       setBanners(banners);
-      const posts = await getPosts();
+      const posts = await getPosts({ token });
       setPosts(posts);
     })();
   }, []);
@@ -21,27 +26,62 @@ export default function Page() {
   const renderItem: ListRenderItem<IPost> = useCallback(
     ({ item }) => (
       <CardPost
-        imagePath={item.postImageUrl}
-        userImagePath={item.userImageUrl}
-        nameInstitution={item.nameInstitution}
-        description={item.description}
-        timeAgo={item.timeAgo}
+        imagePath={item.imageUrl}
+        userImagePath={item.institution.avatar}
+        nameInstitution={item.institution.name}
+        description={item.caption}
+        timeAgo={timeAgo(item.updatedAt)}
         isSavedInitial={false}
       />
     ),
     []
   );
+  function timeAgo(dateString: string): string {
+    const date = parseISO(dateString);
+    const now = new Date();
+    const differenceInMinutes = Math.floor(
+      (now.getTime() - date.getTime()) / 60000
+    );
+
+    if (differenceInMinutes < 60) {
+      return `${differenceInMinutes} minutos atrás`;
+    }
+
+    const differenceInHours = Math.floor(differenceInMinutes / 60);
+    if (differenceInHours < 24) {
+      return `${differenceInHours} horas atrás`;
+    }
+
+    const differenceInDays = Math.floor(differenceInHours / 24);
+    if (differenceInDays < 30) {
+      if (differenceInDays === 1) {
+        return `${differenceInDays} dia atrás`;
+      }
+      return `${differenceInDays} dias atrás`;
+    }
+
+    const differenceInMonths = Math.floor(differenceInDays / 30);
+    if (differenceInMonths < 12) {
+      return `${differenceInMonths} meses atrás`;
+    }
+
+    const differenceInYears = Math.floor(differenceInMonths / 12);
+    return `${differenceInYears} anos atrás`;
+  }
 
   return (
     <View className="flex-1 bg-white px-2 pt-1">
-      <Button
-        size="small"
-        textColor="text-white"
-        customStyles="mb-2 justify-center bg-color_third"
-        onPress={() => router.push('/donate')}
-      >
-        Doar para instituições sociais
-      </Button>
+      {isDonor && (
+        <Button
+          size="small"
+          textColor="text-white"
+          customStyles="mb-2 justify-center bg-color_third"
+          onPress={() => router.push('/donate')}
+        >
+          Doar para instituições sociais
+        </Button>
+      )}
+
       <FlatList
         ListHeaderComponent={<Carousel banners={banners} />}
         data={posts}
