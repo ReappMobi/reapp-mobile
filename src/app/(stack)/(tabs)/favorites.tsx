a5b2cb65dd3a52b5b54251d826fef96e0dfa1a1b
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-07-29 00:47:30 -0300

    refactor: add getFavoriteProjects function in favorites screen

--- src/app/(stack)/(tabs)/favorites.tsx
+++ src/app/(stack)/(tabs)/favorites.tsx
@@ -1,29 +1,50 @@
 import { router, useNavigation } from 'expo-router';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { View, FlatList } from 'react-native';
 import { CardInstitutionProject } from 'src/components';
 import colors from 'src/constants/colors';
+import { useAuth } from 'src/hooks/useAuth';
+import { toggleFavoriteProject } from 'src/services/app-core';
 import { getFavoritesProjects } from 'src/services/user';
 import { IProject } from 'src/types';
 
 const Page = () => {
   const navigation = useNavigation();
-
+  const auth = useAuth();
   const [favoritesProjects, setFavoritesProjects] = useState<IProject[]>([]);
 
   useEffect(() => {
-    navigation.setOptions({
-      headerTitle: 'Favoritos',
-      headerTitleAlign: 'center',
-      headerTitleStyle: {
-        fontSize: 20,
-        fontFamily: 'reapp_bold',
-        color: colors.color_primary,
-      },
-    });
-    getFavoritesProjects().then((response) => {
-      setFavoritesProjects(response);
-    });
+    (async () => {
+      navigation.setOptions({
+        headerTitle: 'Favoritos',
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          fontSize: 20,
+          fontFamily: 'reapp_bold',
+          color: colors.color_primary,
+        },
+      });
+      const token = await auth.getToken();
+      const res = await getFavoritesProjects(auth.user.id, token);
+      setFavoritesProjects(res);
+    })();
+  }, []);
+
+  const handleFavoriteClick = useCallback(async (item: IProject) => {
+    try {
+      const token = await auth.getToken();
+      await toggleFavoriteProject({
+        projectId: item.id,
+        token,
+        donorId: auth.user.id,
+      });
+
+      setFavoritesProjects((prevFavoriteProjects) => {
+        return prevFavoriteProjects.filter((project) => project.id !== item.id);
+      });
+    } catch (error) {
+      console.error('Error toggling favorite project:', error);
+    }
   }, []);
 
   return (
@@ -33,7 +54,7 @@ const Page = () => {
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => (
           <CardInstitutionProject
-            imagePath={item.image}
+            imagePath={item.cover}
             title={item.name}
             textButton="Ver"
             isFavoriteCard
@@ -43,6 +64,9 @@ const Page = () => {
                 params: { projectId: item.id },
               })
             }
+            onPressLike={() => {
+              handleFavoriteClick(item);
+            }}
           />
         )}
         ItemSeparatorComponent={() => <View className="mb-2.5" />}
