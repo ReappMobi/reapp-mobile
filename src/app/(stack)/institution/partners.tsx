87ef224dec7e4aa17c016397bac61ec2572b0143
Gabriel Bastos <gbastosrabelo@gmail.com>
2024-07-29 02:41:55 -0300

    refactor: update partner institution screen to call getPartnersById function

--- src/app/(stack)/institution/partners.tsx
+++ src/app/(stack)/institution/partners.tsx
@@ -1,50 +1,21 @@
+import { useRoute } from '@react-navigation/native';
 import React, { useEffect, useState, memo } from 'react';
 import { View, FlatList } from 'react-native';
 import PartnerCard from 'src/components/PartnerCard';
-import { IPartner } from 'src/types/IPartner';
-
-const data: IPartner[] = [
-  {
-    id: 1,
-    institutionId: 2,
-    name: 'Parceiro 1',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_n0QGjLFH6ToZAeIuE_WTs_FskyXPdypB3A&usqp=CAU',
-  },
-  {
-    id: 2,
-    institutionId: 2,
-    name: 'Parceiro 2',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_n0QGjLFH6ToZAeIuE_WTs_FskyXPdypB3A&usqp=CAU',
-  },
-  {
-    id: 3,
-    institutionId: 1,
-    name: 'Parceiro 3',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_n0QGjLFH6ToZAeIuE_WTs_FskyXPdypB3A&usqp=CAU',
-  },
-  {
-    id: 4,
-    institutionId: 2,
-    name: 'Parceiro 4',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_n0QGjLFH6ToZAeIuE_WTs_FskyXPdypB3A&usqp=CAU',
-  },
-  {
-    id: 5,
-    institutionId: 2,
-    name: 'Parceiro 5',
-    image:
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_n0QGjLFH6ToZAeIuE_WTs_FskyXPdypB3A&usqp=CAU',
-  },
-];
+import { useAuth } from 'src/hooks/useAuth';
+import { getPartnerById } from 'src/services/app-core';
 
 function Partners() {
   const [partners, setPartners] = useState([]);
+  const route = useRoute();
+  const { id } = route.params as { id: number };
+  const auth = useAuth();
   useEffect(() => {
-    setPartners(data);
+    (async () => {
+      const token = await auth.getToken();
+      const res = await getPartnerById(id, token);
+      setPartners(res);
+    })();
   }, []);
 
   return (
@@ -55,7 +26,7 @@ function Partners() {
         keyExtractor={(item) => item.id.toString()}
         numColumns={2}
         renderItem={({ item }) => (
-          <PartnerCard image={item.image} name={item.name} />
+          <PartnerCard image={item.avatar} name={item.name} />
         )}
         ItemSeparatorComponent={() => <View className="h-4" />}
       />
