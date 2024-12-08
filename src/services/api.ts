aa13e19afb520d5b3e28210b946041d7a58ed9cb
Andre0n <andreon098@gmail.com>
2024-12-08 15:22:22 -0300

    feat: update global api usage object

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,11 @@ import config from '../config';
 
 const api = axios.create({
   baseURL: config.API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  validateStatus() {
+    return true;
+  },
 });
-
 export default api;
