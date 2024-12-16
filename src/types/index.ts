4bd4dd35d771b275419288f5078110a8e16637fa
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2024-12-15 21:30:46 -0300

    feat: add SignUpData type

--- src/types/index.ts
+++ src/types/index.ts
@@ -18,6 +18,16 @@ export type SignInData = {
   password: string;
 };
 
+export type SignUpData = {
+  accountType: 'DONOR' | 'INSTITUTION';
+  email: string;
+  name: string;
+  phone?: string;
+  password: string;
+  cnpj?: string;
+  category?: string;
+};
+
 export type { IInstitution };
 export type { IPost } from './IPost';
 export type { IProject } from './IProject';
