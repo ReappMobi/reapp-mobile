49594a98c27cb3f446174fc8a448867c8cb68a21
Andre0n <andreon098@gmail.com>
2025-01-05 11:00:24 -0300

    feat: add max timeout for api request

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,13 +2,20 @@ import axios from 'axios';
 
 import config from '../config';
 
+// TODO: Test real cases to define the best timeout value
+const MAX_TIMEOUT = 5 * 1000; // 5 seconds
+const TIMEOUT_ERROR_MESSAGE = 'Erro ao conectar com o servidor';
+
 const api = axios.create({
   baseURL: config.API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
+  timeout: MAX_TIMEOUT,
+  timeoutErrorMessage: TIMEOUT_ERROR_MESSAGE,
   validateStatus() {
     return true;
   },
 });
+
 export default api;
