2cd056c8d29f2cdf8d0c2de15baa21d3e1d10a67
Andre0n <andreon098@gmail.com>
2025-01-06 09:00:17 -0300

    feat: add check if media is null

--- src/app/(stack)/(drawer)/profile.tsx
+++ src/app/(stack)/(drawer)/profile.tsx
@@ -11,12 +11,12 @@ const DrawerContent = () => {
     <View className="max-w-64 w-full flex-1 rounded-none">
       <View className="bg-color_primary px-6 pt-8">
         <HeaderStatisticsProfile
-          image={user?.media.remoteUrl}
-          name={user?.name}
+          image={user.media?.remoteUrl || ''}
+          name={user?.name || ''}
           donationsQty={0}
           followingQty={(user && user.followersCount) || 0}
           followersQty={(user && user.followingCount) || 0}
-          note={user?.note}
+          note={user?.note || ''}
         />
       </View>
       <View className="w-full flex-1 bg-white px-6">
