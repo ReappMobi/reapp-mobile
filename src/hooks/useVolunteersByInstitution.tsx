c06874ebe6ae5e41b2728a42c30fdca39fb6866f
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2025-01-07 22:29:26 -0300

    feat: add hooks to get institution information

---
+++ src/hooks/useVolunteersByInstitution.tsx
@@ -0,0 +1,55 @@
+import { useCallback, useEffect, useState } from 'react';
+import { getVolunteersByInstitutionId } from 'src/services/app-core';
+import { IPartner } from 'src/types/IPartner';
+
+import { useAuth } from './useAuth';
+
+export const useVolunteersByInstitution = (institutionId: number) => {
+  const auth = useAuth();
+  const [volunteers, setVolunteers] = useState<IPartner[]>([]);
+  const [error, setError] = useState<Error | null>(null);
+  const [refreshing, setRefreshing] = useState<boolean>(false);
+  const [token, setToken] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const fetchData = useCallback(async () => {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const token = await auth.getToken();
+      setToken(token);
+
+      const volunteers = await getVolunteersByInstitutionId(
+        institutionId,
+        token
+      );
+      setVolunteers(volunteers);
+    } catch (err) {
+      setError(err as Error);
+    } finally {
+      setLoading(false);
+    }
+  }, [auth]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    try {
+      await fetchData();
+    } finally {
+      setRefreshing(false);
+    }
+  }, [fetchData]);
+
+  return {
+    volunteers,
+    token,
+    error,
+    loading,
+    refreshing,
+    onRefresh,
+  };
+};
