a4845babfb298e0b640cd91d89b9e23c6dd5d895
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2025-01-07 22:31:55 -0300

    feat: update Collaborator type to add memberType and media parameter

--- src/types/ICollaborator.ts
+++ src/types/ICollaborator.ts
@@ -1,6 +1,9 @@
+import { IMedia } from './IMedia';
+
 export interface ICollaborator {
   id: number;
   institutionId: number;
   name: string;
-  image: string;
+  memberType: string;
+  media: IMedia;
 }
