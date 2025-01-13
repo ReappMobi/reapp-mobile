81f923939f3d8bca44b6138077a8a55740249189
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2025-01-13 02:05:00 -0300

    feat: update saved posts screen

--- src/app/(stack)/(profile)/saved.tsx
+++ src/app/(stack)/(profile)/saved.tsx
@@ -1,39 +1,184 @@
-import { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { parseISO } from 'date-fns';
+import { memo, useCallback } from 'react';
+import {
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  TouchableOpacity,
+  ListRenderItem,
+  RefreshControl,
+} from 'react-native';
 import { CardPost } from 'src/components';
-import { getSavedPosts } from 'src/services/user';
+import { useAuth } from 'src/hooks/useAuth';
+import { useSavedPosts } from 'src/hooks/useSavedPosts';
+import {
+  likePost,
+  savePost,
+  unlikePost,
+  unsavePost,
+} from 'src/services/app-core';
+import { IPost } from 'src/types';
 
-const SavedPage = () => {
-  const [savedPosts, setSavedPosts] = useState([]);
+type PostItemProps = {
+  item: IPost;
+  token: string | null;
+  userId?: string | number;
+};
 
-  useEffect(() => {
-    getSavedPosts().then((response) => {
-      setSavedPosts(response);
-    });
-  }, []);
+const PostItem = memo<PostItemProps>(({ item, token, userId }) => {
+  const isLiked = item.likes?.some((like) => like.accountId === userId);
+  const isSaved = item.saves?.some((save) => save.accountId === userId);
+  const handleLike = useCallback(() => {
+    if (token) {
+      likePost({ id: item.id, token });
+    }
+  }, [item.id, token]);
+
+  const handleUnlike = useCallback(() => {
+    if (token) {
+      unlikePost({ id: item.id, token });
+    }
+  }, [item.id, token]);
+
+  const handleSave = useCallback(() => {
+    if (token) {
+      savePost({ id: item.id, token });
+    }
+  }, [item.id, token]);
+
+  const handleUnsave = useCallback(() => {
+    if (token) {
+      unsavePost({ id: item.id, token });
+    }
+  }, [item.id, token]);
+
+  const timeString = timeAgo(item.updatedAt);
 
   return (
-    <View className="bg-white px-2 pb-12">
+    <CardPost
+      mediaUrl={item.media?.remoteUrl || ''}
+      mediaBlurhash={item.media?.blurhash || ''}
+      userImageUrl={item.institution?.account?.media?.remoteUrl || ''}
+      userImageBlurhash={item.institution?.account?.media?.blurhash || ''}
+      nameInstitution={item.institution?.account?.name || ''}
+      description={item.body || ''}
+      timeAgo={timeString}
+      isSavedInitial={isSaved}
+      isLikedInitial={isLiked}
+      onPressLike={handleLike}
+      onPressUnlike={handleUnlike}
+      onPressSave={handleSave}
+      onPressUnSave={handleUnsave}
+    />
+  );
+});
+
+function timeAgo(dateString: string): string {
+  const date = parseISO(dateString);
+
+  const now = new Date();
+  const differenceInMinutes = Math.floor(
+    (now.getTime() - date.getTime()) / 60000
+  );
+
+  if (differenceInMinutes <= 1) {
+    return `${differenceInMinutes} minuto atrás`;
+  }
+
+  if (differenceInMinutes < 60) {
+    return `${differenceInMinutes} minutos atrás`;
+  }
+
+  const differenceInHours = Math.floor(differenceInMinutes / 60);
+  if (differenceInHours < 24) {
+    return `${differenceInHours} horas atrás`;
+  }
+
+  const differenceInDays = Math.floor(differenceInHours / 24);
+  if (differenceInDays < 30) {
+    if (differenceInDays === 1) {
+      return `${differenceInDays} dia atrás`;
+    }
+    return `${differenceInDays} dias atrás`;
+  }
+
+  const differenceInMonths = Math.floor(differenceInDays / 30);
+  if (differenceInMonths < 12) {
+    return `${differenceInMonths} meses atrás`;
+  }
+
+  const differenceInYears = Math.floor(differenceInMonths / 12);
+  return `${differenceInYears} anos atrás`;
+}
+
+function PostList() {
+  const { user, token } = useAuth();
+  const { posts, loading, error, refreshing, onRefresh } = useSavedPosts();
+
+  if (loading) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" color="#000" />
+      </View>
+    );
+  }
+
+  if (!loading && error) {
+    return (
+      <View className="flex-1 items-center justify-center p-4">
+        <Text className="mb-2 text-lg font-bold text-red-500">
+          Ocorreu um erro!
+        </Text>
+        <Text>{error?.message || 'Ocorreu um erro ao carregar os posts'}</Text>
+        <TouchableOpacity onPress={onRefresh}>
+          <Text className="mt-4 text-blue-500">Tentar novamente</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  const renderItem: ListRenderItem<IPost> = ({ item }) => (
+    <PostItem item={item} token={token} userId={user?.id} />
+  );
+
+  return (
+    <FlatList
+      className="flex-1"
+      data={posts}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id.toString()}
+      showsVerticalScrollIndicator={false}
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          colors={['#ff0000']} // Android
+          tintColor="#0000ff" // iOS
+          title="Recarregando..." // iOS
+        />
+      }
+      ItemSeparatorComponent={() => (
+        <View className="mt-4 h-[2px] w-full bg-slate-200" />
+      )}
+      ListEmptyComponent={
+        <View className="flex-1 items-center justify-center p-4">
+          <Text className="font-reapp_medium text-base">
+            Nenhuma postagem salva.
+          </Text>
+        </View>
+      }
+    />
+  );
+}
+
+const SavedPage = () => {
+  return (
+    <View className="flex-1 px-2 pb-12">
       <Text className="my-2 text-center font-reapp_medium text-xl text-text_primary">
         Postagens salvas
       </Text>
-      {savedPosts.length === 0 ? (
-        <Text>Carregando...</Text>
-      ) : (
-        <FlatList
-          data={savedPosts}
-          renderItem={({ item }) => (
-            <CardPost
-              imagePath={item.postImageUrl}
-              userImagePath={item.userImageUrl}
-              nameInstitution={item.nameInstitution}
-              description={item.description}
-              timeAgo={item.timeAgo}
-              isSavedInitial
-            />
-          )}
-        />
-      )}
+      <PostList />
     </View>
   );
 };
