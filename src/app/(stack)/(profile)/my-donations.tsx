13ec23d4372819bc3bd437ed062b635e0b0712f3
Andre0n <andreon098@gmail.com>
2025-01-18 11:47:29 -0300

    feat: update `my-donations-page`
    - Load donation from new `getDonorDonations` service.
    - Improve render logic.
    - Improve render card.

--- src/app/(stack)/(profile)/my-donations.tsx
+++ src/app/(stack)/(profile)/my-donations.tsx
@@ -1,18 +1,40 @@
 import { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, Alert } from 'react-native';
 import { DonationInformationItem } from 'src/components';
-import { getDonations } from 'src/services/user';
+import { useAuth } from 'src/hooks/useAuth';
+import { getDonationsByDonor } from 'src/services/donations';
+import { Donation } from 'src/types/IDonation';
+import { timeAgo } from 'src/utils/time-ago';
 
 const MyDonationsPage = () => {
-  const [donations, setDonations] = useState([]);
+  const { user, token } = useAuth();
+
+  const [loading, setLoading] = useState(true);
+  const [donations, setDonations] = useState<Donation[]>([]);
+  const [page, setPage] = useState(1);
+
+  const fetchDonations = async () => {
+    setLoading(true);
+    try {
+      const donations = await getDonationsByDonor(page, user.id, token);
+
+      console.log(donations);
+
+      setDonations([...donations]);
+      setPage(page + 1);
+    } catch (error) {
+      Alert.alert('Erro', error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    getDonations().then((response) => {
-      setDonations(response);
-    });
+    fetchDonations();
+    setLoading(false);
   }, []);
 
-  return donations.length === 0 ? (
+  return loading ? (
     <Text>Carregando...</Text>
   ) : (
     <View className="px-4 pb-6">
@@ -20,15 +42,36 @@ const MyDonationsPage = () => {
         Minhas doações
       </Text>
 
+      {donations.length === 0 && (
+        <Text className="mt-6 text-center">
+          Você ainda não fez nenhuma doação.
+        </Text>
+      )}
+
       <FlatList
         data={donations}
-        renderItem={({ item }) => (
-          <DonationInformationItem
-            title={item.title}
-            subtitle={item.subtitle}
-            image="https://placehold.co/600x400/png"
-          />
-        )}
+        onEndReached={() => fetchDonations()}
+        renderItem={({ item: donation }) => {
+          const destination =
+            donation.project?.name || donation.institution?.account.name;
+
+          const media =
+            donation.project?.media?.remoteUrl ||
+            donation.institution?.account.media?.remoteUrl;
+
+          const formatedAmount = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(+donation.amount);
+
+          return (
+            <DonationInformationItem
+              title={`Você doou ${formatedAmount} para ${destination}`}
+              subtitle={`Há ${timeAgo(donation.createdAt)}`}
+              image={media}
+            />
+          );
+        }}
       />
     </View>
   );
