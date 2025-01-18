a489e4e4c26d4cf9b84364dec1b14525455cfa1f
Andre0n <andreon098@gmail.com>
2025-01-18 17:42:28 -0300

    feat: update `my-donations` page to render institution donations

--- src/app/(stack)/(profile)/my-donations.tsx
+++ src/app/(stack)/(profile)/my-donations.tsx
@@ -2,13 +2,58 @@ import { useEffect, useState } from 'react';
 import { View, Text, FlatList, Alert } from 'react-native';
 import { DonationInformationItem } from 'src/components';
 import { useAuth } from 'src/hooks/useAuth';
-import { getDonationsByDonor } from 'src/services/donations';
+import {
+  getDonationsByDonor,
+  getInstitutionDonations,
+} from 'src/services/donations';
 import { Donation } from 'src/types/IDonation';
 import { timeAgo } from 'src/utils/time-ago';
 
-const MyDonationsPage = () => {
-  const { user, token } = useAuth();
+const renderDonorDonations = (donation: Donation) => {
+  const destination =
+    donation.project?.name || donation.institution?.account.name;
 
+  const media =
+    donation.project?.media.remoteUrl ||
+    donation.institution?.account.media.remoteUrl;
+
+  const formatedAmount = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(+donation.amount);
+
+  const message = `Você doou ${formatedAmount} para ${destination}`;
+
+  return (
+    <DonationInformationItem
+      title={message}
+      subtitle={`Há ${timeAgo(donation.createdAt)}`}
+      image={media}
+    />
+  );
+};
+
+const renderInstitutionDonations = (donation: Donation) => {
+  const origin = donation.donor.account.name;
+  const media = donation.donor.account.media;
+  const destination = donation.project?.name || 'sua instituição';
+  const formatedAmount = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(+donation.amount);
+
+  const message = `${origin} doou ${formatedAmount} para ${destination}`;
+  return (
+    <DonationInformationItem
+      title={message}
+      subtitle={`Há ${timeAgo(donation.createdAt)}`}
+      image={media}
+    />
+  );
+};
+
+const MyDonationsPage = () => {
+  const { user, token, isDonor } = useAuth();
   const [loading, setLoading] = useState(true);
   const [donations, setDonations] = useState<Donation[]>([]);
   const [page, setPage] = useState(1);
@@ -16,10 +61,9 @@ const MyDonationsPage = () => {
   const fetchDonations = async () => {
     setLoading(true);
     try {
-      const donations = await getDonationsByDonor(page, user.id, token);
-
-      console.log(donations);
-
+      let donations = [];
+      if (isDonor) donations = await getDonationsByDonor(page, user.id, token);
+      else donations = await getInstitutionDonations(page, token);
       setDonations([...donations]);
       setPage(page + 1);
     } catch (error) {
@@ -41,36 +85,21 @@ const MyDonationsPage = () => {
       <Text className="my-2 text-center font-reapp_medium text-xl text-text_primary">
         Minhas doações
       </Text>
-
       {donations.length === 0 && (
         <Text className="mt-6 text-center">
-          Você ainda não fez nenhuma doação.
+          {isDonor
+            ? 'Você ainda não fez nenhuma doação'
+            : 'Você ainda não recebeu nenhuma doação'}
         </Text>
       )}
 
       <FlatList
         data={donations}
         onEndReached={() => fetchDonations()}
-        renderItem={({ item: donation }) => {
-          const destination =
-            donation.project?.name || donation.institution?.account.name;
-
-          const media =
-            donation.project?.media?.remoteUrl ||
-            donation.institution?.account.media?.remoteUrl;
-
-          const formatedAmount = new Intl.NumberFormat('pt-BR', {
-            style: 'currency',
-            currency: 'BRL',
-          }).format(+donation.amount);
-
-          return (
-            <DonationInformationItem
-              title={`Você doou ${formatedAmount} para ${destination}`}
-              subtitle={`Há ${timeAgo(donation.createdAt)}`}
-              image={media}
-            />
-          );
+        renderItem={({ item }) => {
+          return isDonor
+            ? renderDonorDonations(item)
+            : renderInstitutionDonations(item);
         }}
       />
     </View>
