16a21c9060e74863ec0657be7ba4bc14776265e4
Andre0n <andreon098@gmail.com>
2025-01-18 11:45:25 -0300

    feat: add Donation interface

---
+++ src/types/IDonation.ts
@@ -0,0 +1,25 @@
+import { IInstitution } from './IInstitution';
+import { IProject } from './IProject';
+
+enum DonationStatus {
+  PENDING,
+  APPROVED,
+  REJECTED,
+  FAILED,
+  CANCELLED,
+}
+
+export interface Donation {
+  id: number;
+  amount: string;
+  createdAt: string;
+  updatedAt: string;
+  status: DonationStatus;
+  paymentTransactionId: string;
+  paymentCheckoutUrl: string;
+  projectId?: number;
+  institutionId?: number;
+  donorId: number;
+  institution?: IInstitution;
+  project?: IProject;
+}
