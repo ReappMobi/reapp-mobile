89297b53e5090112a43b79821c9343df9c07bcc1
Andre0n <andreon098@gmail.com>
2025-01-18 17:41:24 -0300

    feat(donations.ts): add `getInstitutionDonations` function

--- src/services/donations.ts
+++ src/services/donations.ts
@@ -26,3 +26,25 @@ export const getDonationsByDonor = async (
     throw new Error(error.message);
   }
 };
+
+export const getInstitutionDonations = async (
+  page: number = 1,
+  token: string
+): Promise<Donation[]> => {
+  try {
+    const response = await api.get(
+      `/donation/institution?page=${page}&limit=${DONATIONS_LIST_LIMIT}`,
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+
+    if (response.status !== 200) {
+      throw new Error(response.data.message || 'Erro ao buscar doações');
+    }
+
+    if (response.data) {
+      return response.data;
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
