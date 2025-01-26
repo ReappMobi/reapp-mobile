fb5eb79be018645e9981b155aa9331943af218c6
Andre0n <andreon098@gmail.com>
2025-01-26 13:19:31 -0300

    feat: add `functools.ts` file

---
+++ src/utils/functools.ts
@@ -0,0 +1,7 @@
+export const debounce = (fn: Function, delay: number) => {
+  let timeoutId: NodeJS.Timeout;
+  return function (...args: any[]) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn(...args), delay);
+  };
+};
