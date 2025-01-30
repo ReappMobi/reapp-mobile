35549329dde2ae3b830b07e1c9ddec8bb368ba74
Andre0n <andreon098@gmail.com>
2025-01-30 09:51:12 -0300

    feat(hooks): add `useCamera` hook

---
+++ src/hooks/useCamera.ts
@@ -0,0 +1,30 @@
+import { Camera } from 'expo-camera';
+import { launchCameraAsync, MediaType } from 'expo-image-picker';
+import { Alert } from 'react-native';
+
+type CallbackFn = (uri: string) => void;
+
+const requestCameraPermission = async () => {
+  const { status } = await Camera.requestCameraPermissionsAsync();
+  if (status !== 'granted') {
+    Alert.alert('Precisamos de permissão para acessar a câmera.');
+  }
+};
+
+const takePicture = async (mediaTypes: MediaType[], callback: CallbackFn) => {
+  await requestCameraPermission();
+  const result = await launchCameraAsync({
+    mediaTypes,
+    allowsEditing: false,
+    aspect: [4, 3],
+    quality: 1,
+  });
+
+  if (!result.canceled) {
+    callback(result.assets[0].uri);
+  }
+};
+
+export const useCamera = () => {
+  return { takePicture };
+};
