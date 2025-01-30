c7107152be117a79531800e8feb33aebd6543bb8
Andre0n <andreon098@gmail.com>
2025-01-30 12:09:49 -0300

    feat(services): add `createProject` function

--- src/services/project.ts
+++ src/services/project.ts
@@ -1,3 +1,5 @@
+import { ApiResponseError } from 'src/errors/ApiResponseError';
+import { ApiErrorResponse } from 'src/types/IApiResponseError';
 import { Category } from 'src/types/ICategory';
 
 import api from './api';
@@ -20,5 +22,61 @@ export const getProjectsCategories = async (
   } catch (error) {
     console.log(error);
     throw error;
+  }
+};
+
+type CreateProject = {
+  name: string;
+  subtitle: string;
+  description: string;
+  category: string;
+  media: string;
+};
+
+type CreateProjectResponse = {
+  description: string;
+  name: string;
+  media: string;
+  subtitle: string;
+  category: Category;
+  institutionId: number;
+  accountId: number;
+};
+
+export const createProject = async (
+  token: string,
+  data: CreateProject
+): Promise<[CreateProjectResponse | null, ApiErrorResponse | Error | null]> => {
+  const formData = new FormData();
+  for (const key in data) {
+    if (key === 'media') {
+      formData.append('media', {
+        uri: data[key],
+        type: 'image/jpeg',
+        name: 'photo.jpg',
+      } as any);
+      continue;
+    }
+    formData.append(key, data[key]);
+  }
+
+  try {
+    const response = await api.post('/project', formData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    if (response.status !== 201) {
+      const { error, message, statusCode } = response.data;
+      throw new ApiResponseError(error, message, statusCode);
+    }
+    return [response.data, null];
+  } catch (error) {
+    console.log(error);
+    if (error instanceof ApiResponseError) {
+      return [null, error];
+    }
+    return [null, new Error('Erro ao criar projeto')];
   }
 };
