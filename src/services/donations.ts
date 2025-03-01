f402f385c9fd1456fa5f548191c5d2c4f55a5ef5
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2025-02-28 22:00:53 -0300

    feat: update donation service

--- src/services/donations.ts
+++ src/services/donations.ts
@@ -1,5 +1,3 @@
-import { Donation } from 'src/types/IDonation';
-
 import api from './api';
 
 const DONATIONS_LIST_LIMIT = 10;
@@ -7,11 +5,12 @@ const DONATIONS_LIST_LIMIT = 10;
 export const getDonationsByDonor = async (
   donorId: number,
   page: number = 1,
-  token: string
-): Promise<Donation[]> => {
+  token: string,
+  period: string
+) => {
   try {
     const response = await api.get(
-      `/donation/donor/${donorId}?page=${page}&limit=${DONATIONS_LIST_LIMIT}`,
+      `/donation/donor/${donorId}?page=${page}&limit=${DONATIONS_LIST_LIMIT}&period=${period}`,
       { headers: { Authorization: `Bearer ${token}` } }
     );
 
@@ -29,11 +28,12 @@ export const getDonationsByDonor = async (
 
 export const getInstitutionDonations = async (
   page: number = 1,
+  period: string,
   token: string
-): Promise<Donation[]> => {
+) => {
   try {
     const response = await api.get(
-      `/donation/institution?page=${page}&limit=${DONATIONS_LIST_LIMIT}`,
+      `/donation/institution?page=${page}&limit=${DONATIONS_LIST_LIMIT}&period=${period}`,
       { headers: { Authorization: `Bearer ${token}` } }
     );
 
@@ -46,5 +46,51 @@ export const getInstitutionDonations = async (
     }
   } catch (error) {
     throw new Error(error.message);
+  }
+};
+
+export const getGeneralDonations = async (
+  page: number = 1,
+  period: string,
+  token: string
+) => {
+  try {
+    const response = await api.get(
+      `/donation/institution/general?page=${page}&limit=${DONATIONS_LIST_LIMIT}&period=${period}`,
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+
+    if (response.status !== 200) {
+      throw new Error(response.data.message || 'Erro ao buscar doações');
+    }
+
+    if (response.data) {
+      return response.data;
+    }
+  } catch (error) {
+    throw new error(error.message);
+  }
+};
+
+export const getProjectDonations = async (
+  page: number = 1,
+  period: string,
+  token: string
+) => {
+  try {
+    const response = await api.get(
+      `/donation/institution/projects?page=${page}&limit=${DONATIONS_LIST_LIMIT}&period=${period}`,
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+
+    if (response.status !== 200) {
+      throw new Error(response.data.message || 'Erro ao buscar doações');
+    }
+
+    if (response.data) {
+      return response.data;
+    }
+  } catch (error) {
+    throw new error(error.message);
   }
 };
