87b93500c927a6c98b41aea4e6f389c0c64d21b8
Gabriel Bastos Rabelo <gbastosrabelo@gmail.com>
2025-03-02 13:28:54 -0300

    feat: add delete project button institution profile

--- src/app/(stack)/(tabs)/profile/projects.tsx
+++ src/app/(stack)/(tabs)/profile/projects.tsx
@@ -10,15 +10,19 @@ import {
   TouchableOpacity,
   ListRenderItem,
   RefreshControl,
+  Alert,
 } from 'react-native';
 import { Button } from 'src/components';
 import CardInstitutionProject from 'src/components/CardInstitutionProject';
 import colors from 'src/constants/colors';
 import { useProjectsByInstitution } from 'src/hooks/useProjectsByInstitution';
+import { deleteProject } from 'src/services/app-core';
 import { IProject } from 'src/types';
 
 type ProjectListProps = {
   item: IProject;
+  token: string;
+  onDelete: (projectId: number) => void;
 };
 
 const renderHeader = () => (
@@ -35,10 +39,38 @@ const renderHeader = () => (
   </View>
 );
 
-const ProjectItem = memo<ProjectListProps>(({ item }) => {
+const ProjectItem = memo<ProjectListProps>(({ item, token, onDelete }) => {
   const handleCardClick = useCallback((projectId: number) => {
     router.navigate({ pathname: 'project', params: { projectId } });
   }, []);
+
+  const handleDelete = useCallback(() => {
+    if (token) {
+      Alert.alert(
+        'Confirmar exclusão',
+        'Tem certeza que deseja excluir esta postagem?',
+        [
+          {
+            text: 'Cancelar',
+            style: 'cancel',
+          },
+          {
+            text: 'Confirmar',
+            onPress: () => {
+              deleteProject({ id: item.id, token })
+                .then(() => onDelete(item.id))
+                .catch((error) => {
+                  Alert.alert('Erro', 'Não foi possível excluir a postagem');
+                  console.error(error);
+                });
+            },
+            style: 'destructive',
+          },
+        ],
+        { cancelable: true }
+      );
+    }
+  }, [item.id, token, onDelete]);
   return (
     <CardInstitutionProject
       imagePath={item.media?.remoteUrl}
@@ -46,13 +78,25 @@ const ProjectItem = memo<ProjectListProps>(({ item }) => {
       subtitle={item.subtitle}
       textButton="Ver mais detalhes"
       onPress={() => handleCardClick(item.id)}
+      onPressDelete={handleDelete}
     />
   );
 });
 
 function ProjectList({ institutionId }) {
-  const { projects, token, error, loading, refreshing, onRefresh } =
-    useProjectsByInstitution(institutionId);
+  const {
+    projects,
+    setProjects,
+    token,
+    error,
+    loading,
+    refreshing,
+    onRefresh,
+  } = useProjectsByInstitution(institutionId);
+
+  const handleDeleteProject = useCallback((projectId: number) => {
+    setProjects((prev) => prev.filter((project) => project.id !== projectId));
+  }, []);
 
   if (loading && !token) {
     return (
@@ -64,7 +108,7 @@ function ProjectList({ institutionId }) {
 
   const renderItem: ListRenderItem<IProject> = ({ item }) =>
     projects.length > 0 ? (
-      <ProjectItem item={item} />
+      <ProjectItem item={item} onDelete={handleDeleteProject} token={token} />
     ) : (
       <View className="flex-1 items-center justify-center p-4">
         <Text className="font-reapp_medium text-base">
@@ -81,7 +125,6 @@ function ProjectList({ institutionId }) {
         </Text>
         <Text>{error.message}</Text>
 
-        {/* Botão para tentar novamente */}
         <TouchableOpacity onPress={onRefresh}>
           <Text className="mt-4 text-blue-500">Tentar novamente</Text>
         </TouchableOpacity>
