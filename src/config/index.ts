11a3e764432b50b5034733c0fbf95bf0231c8eba
André Gabriel <andreon098@gmail.com>
2025-08-09 11:42:20 -0300

    feat: replace config with .env

--- src/config/index.ts
+++
@@ -1,6 +0,0 @@
-const config = {
-  API_URL: 'http://10.74.0.19:3000',
-  ENV: 'development',
-};
-
-export default config;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
-import config from '../config';
 
 // TODO: Test real cases to define the best timeout value
 const MAX_TIMEOUT = 5 * 1000; // 5 seconds
 const TIMEOUT_ERROR_MESSAGE = 'Erro ao conectar com o servidor';
 
 const api = axios.create({
-  baseURL: config.API_URL,
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
