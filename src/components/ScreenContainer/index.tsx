a9faf75e62dae05e4b3cb5e4beb9a27187b880d5
André Gabriel <andreon098@gmail.com>
2025-09-20 12:43:07 -0300

    refactor: remove unused React import from ScreenContainer

--- src/components/ScreenContainer/index.tsx
+++ src/components/ScreenContainer/index.tsx
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function ScreenContainer({ children }) {
