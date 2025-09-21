8b33e3758e5cedd847ca5af4787b60792a1c1e64
André Gabriel <andreon098@gmail.com>
2025-09-21 10:06:19 -0300

    feat: implement useMediaPicker hook for camera and gallery access

---
+++ src/hooks/useMediaPicker.ts
@@ -0,0 +1,63 @@
+import {
+  ImagePickerAsset,
+  MediaType,
+  launchCameraAsync,
+  launchImageLibraryAsync,
+  requestCameraPermissionsAsync,
+  requestMediaLibraryPermissionsAsync,
+} from 'expo-image-picker';
+import { Alert } from 'react-native';
+
+type CallbackFn = (asset: ImagePickerAsset) => void;
+
+const requestCameraPermission = async () => {
+  const { status } = await requestCameraPermissionsAsync();
+  if (status !== 'granted') {
+    Alert.alert('Precisamos de permissão para acessar a câmera.');
+    return false;
+  }
+  return true;
+};
+
+const requestGalleryPermission = async () => {
+  const { status } = await requestMediaLibraryPermissionsAsync();
+  if (status !== 'granted') {
+    Alert.alert('Precisamos de permissão para acessar a galeria.');
+    return false;
+  }
+  return true;
+};
+
+const takePicture = async (mediaTypes: MediaType[], callback: CallbackFn) => {
+  const hasPermission = await requestCameraPermission();
+  if (!hasPermission) return;
+
+  const result = await launchCameraAsync({
+    mediaTypes,
+    allowsEditing: false,
+    quality: 0.8,
+  });
+
+  if (!result.canceled) {
+    callback(result.assets[0]);
+  }
+};
+
+const pickMedia = async (mediaTypes: MediaType[], callback: CallbackFn) => {
+  const hasPermission = await requestGalleryPermission();
+  if (!hasPermission) return;
+
+  const result = await launchImageLibraryAsync({
+    mediaTypes,
+    allowsEditing: false,
+    quality: 0.8,
+  });
+
+  if (!result.canceled) {
+    callback(result.assets[0]);
+  }
+};
+
+export const useMediaPicker = () => {
+  return { takePicture, pickMedia };
+};
