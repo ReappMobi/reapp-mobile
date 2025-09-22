9f0f2584c0effba0ccdc0d7c49785ea01c380432
André Gabriel <andreon098@gmail.com>
2025-09-22 09:11:14 -0300

    style: format code for improved readability in Input component

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -13,11 +13,13 @@ function Input({
         props.editable === false &&
           cn(
             'opacity-50',
-            Platform.select({ web: 'disabled:pointer-events-none disabled:cursor-not-allowed' })
+            Platform.select({
+              web: 'disabled:pointer-events-none disabled:cursor-not-allowed',
+            })
           ),
         Platform.select({
           web: cn(
-            'placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground outline-none transition-[color,box-shadow] md:text-sm',
+            'placeholder:text-muted-foreground selection:bg-primary outline-none transition-[color,box-shadow] md:text-sm',
             'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
             'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive'
           ),
