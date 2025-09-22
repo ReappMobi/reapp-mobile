718a86f8ab120cb141652549ebb28ed62ace44b9
André Gabriel <andreon098@gmail.com>
2025-09-22 09:11:42 -0300

    refactor: improve layout and text structure in Welcome component

--- src/app/(stack)/(aux)/welcome.tsx
+++ src/app/(stack)/(aux)/welcome.tsx
@@ -1,32 +1,29 @@
-import Ionicons from '@expo/vector-icons/Ionicons';
+import { Button } from '@/components/ui/button';
+import { Icon } from '@/components/ui/icon';
+import { Text } from '@/components/ui/text';
 import { router } from 'expo-router';
-import { Text, View } from 'react-native';
+import { ChevronRight } from 'lucide-react-native';
+import { View } from 'react-native';
 import WelcomePageImage from 'src/assets/images/WelcomePageImage.svg';
-import { Button } from 'src/components';
 import colors from 'src/constants/colors';
 
 export default function Welcome() {
   return (
-    <View className="flex-1  items-center gap-y-8 bg-white p-4">
+    <View className="flex-1 items-center gap-y-4 bg-white p-4">
       <WelcomePageImage width={256} height={183} />
-      <Text className="font-reapp_medium text-base">
+      <Text variant="large" className="text-text_neutral">
         Somos uma organização que busca facilitar o encontro de doadores e
-        parceiros com instituições beneficentes. Vamos começar?
+        parceiros com instituições beneficentes.
+      </Text>
+      <Text variant="large" className="self-start text-text_neutral">
+        Vamos começar?
       </Text>
       <Button
-        endIcon={
-          <Ionicons
-            name="chevron-forward"
-            size={18}
-            color={colors.text_primary}
-          />
-        }
-        customStyles="mt-8 w-64 justify-center gap-x-1"
-        textColor="text-text_primary"
-        textSize="text-lg"
+        className="mt-8 w-full max-w-sm justify-center gap-x-0.5 h-12 bg-color_primary active:bg-color_secundary/90"
         onPress={() => router.replace('/sign-in')}
       >
-        Continuar
+        <Text className="text-white font-bold">Começar</Text>
+        <Icon as={ChevronRight} size={18} color={colors.text_white} />
       </Button>
     </View>
   );
