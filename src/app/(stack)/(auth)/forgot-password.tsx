16d70ae303aea3951496b0888ae1d864248d4c29
André Gabriel <andreon098@gmail.com>
2025-10-20 10:56:18 -0300

    feat: implement forgot password functionality with form handling

--- src/app/(stack)/(auth)/forgot-password.tsx
+++ src/app/(stack)/(auth)/forgot-password.tsx
@@ -1,57 +1,87 @@
+import { ControlledInput, Form } from '@/components';
 import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { useToast } from '@/components/ui/toast';
+import { useSendEmail } from '@/services/auth/request';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { router } from 'expo-router';
 import { cssInterop } from 'nativewind';
-import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Text, View } from 'react-native';
 import ForgetPasswordImage from 'src/assets/images/ForgetPassowordImage.svg';
+import { z } from 'zod';
 
 const ForgetPasswordImageStyled = cssInterop(ForgetPasswordImage, {
   className: 'style',
 });
 
+const schema = z.object({
+  email: z.string().email('Digite um email válido'),
+});
+
+type FormData = z.infer<typeof schema>;
+
 export default function ForgotPassword() {
-  const [email, setEmail] = useState('');
+  const { toast } = useToast();
+  const { mutate: sendEmail, isPending: isSendingEmail } = useSendEmail();
+
+  const form = useForm<FormData>({
+    defaultValues: {
+      email: '',
+    },
+    reValidateMode: 'onSubmit',
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = ({ email }: FormData) => {
+    sendEmail(
+      { email },
+      {
+        onSuccess: ({ token }) => {
+          toast('O código foi enviado para o seu email!');
+          router.replace('recovery-password/' + encodeURIComponent(token));
+        },
+        onError: (error) => {
+          toast('Erro ao enviar o email de recuperação.');
+          console.error('Error sending recovery email:', error);
+        },
+      }
+    );
+  };
 
   return (
-    <View className="gap-y-3 p-4">
+    <Form form={form} onSubmit={onSubmit} className="gap-y-3 p-4">
       <View>
         <Text className="font-reapp_bold text-xl text-text_neutral/90">
           Esqueceu a senha?
         </Text>
       </View>
+
       <View className="mb-4 items-center">
         <ForgetPasswordImageStyled className="w-64 h-72" />
       </View>
 
-      <View>
-        <Input
-          placeholder="Digite o email da sua conta"
-          inputMode="email"
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          keyboardType="email-address"
-          textContentType="emailAddress"
-          autoCapitalize="none"
-          autoCorrect={false}
-          autoFocus
-          className="border-color_primary/60 border-2 focus:border-color_primary rounded-sm h-12"
-        />
-      </View>
+      <ControlledInput
+        name="email"
+        placeholder="Digite o email da sua conta"
+        inputMode="email"
+        keyboardType="email-address"
+        textContentType="emailAddress"
+        autoCapitalize="none"
+        autoCorrect={false}
+        autoFocus
+      />
 
       <View>
         <Button
           className="w-full justify-center h-11 bg-color_primary active:bg-color_secundary/90 disabled:opacity-85 disabled:bg-color_primary/50"
-          onPress={() => {
-            router.replace('recovery-password/' + encodeURIComponent(email));
-          }}
-          disabled={email.length === 0}
+          disabled={isSendingEmail}
+          onPress={form.handleSubmit(onSubmit)}
         >
           <Text className="text-base text-white font-semibold">
             Receber código de verificação
           </Text>
         </Button>
       </View>
-    </View>
+    </Form>
   );
 }
