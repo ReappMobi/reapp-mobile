832d6f8f391552cbe59e48bb43e235c5e19b4e50
André Gabriel <andreon098@gmail.com>
2025-10-27 14:59:03 -0300

    fix: fix url path
    * feat: update baseURL setup
    
    * chore: remove web from app.json
    
    * chore: update dependencies
    
    * feat: remove unused import

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const MAX_TIMEOUT = 5 * 1000; // 5 seconds
 const TIMEOUT_ERROR_MESSAGE = 'Erro ao conectar com o servidor';
 
-const baseURL = process.env.EXPO_PUBLIC_API_URL || 'http://localhost:3000';
+const baseURL = process.env.EXPO_PUBLIC_API_URL || 'https://api.reapp.dev.br';
 
 const api = axios.create({
   baseURL,
