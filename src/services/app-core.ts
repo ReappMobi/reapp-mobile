caa7b0f11bbdb9c25aa80e8edd12b1dfd35c586e
André Gabriel <andreon098@gmail.com>
2025-11-29 10:57:56 -0300

    refactor: remove mock functions

--- src/services/app-core.ts
+++ src/services/app-core.ts
@@ -1,29 +1,7 @@
-import {
-  ICategory,
-  institutionCategories,
-} from 'src/mocks/app-InstitutionCategory-data';
-import { banners } from 'src/mocks/app-banners-data';
-import {
-  IDonationEixo,
-  donationsByEixo,
-} from 'src/mocks/app-donationEixo-data';
-import {
-  IDonationSegment,
-  donationsBySegment,
-} from 'src/mocks/app-donationSegment-data';
 import { IBanner } from 'src/types';
 
 import { RequestMedia } from './account';
 import api from './api';
-
-export async function getSharedCampaigns(): Promise<IBanner[]> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log('fetching banners...');
-      resolve(banners);
-    }, 1000);
-  });
-}
 
 export async function getPosts(data: { token: string }) {
   try {
@@ -487,32 +465,6 @@ export async function getProjectCategories(data) {
   } catch (error) {
     return { error: error.message };
   }
-}
-
-export async function getDonationsByEixo(): Promise<IDonationEixo[]> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log('fetchin donations by field...');
-      resolve(donationsByEixo);
-    }, 1000);
-  });
-}
-
-export async function getDonationsBySegment(): Promise<IDonationSegment[]> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log('fetchin donations by segment...');
-      resolve(donationsBySegment);
-    }, 5000);
-  });
-}
-
-export async function getCategoryById(id: number): Promise<ICategory> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(institutionCategories.find((category) => category.id === id));
-    }, 1000);
-  });
 }
 
 export async function getInstituitionPosts(
