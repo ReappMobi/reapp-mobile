44ae91b8c772b51cf57719bbd6939405ef68f160
André Gabriel <andreon098@gmail.com>
2025-11-29 15:05:54 -0300

    feat: replace form by controlled form

--- src/app/(stack)/(auth)/sign-in.tsx
+++ src/app/(stack)/(auth)/sign-in.tsx
@@ -11,27 +11,24 @@ import {
   signInFormData,
   signInFormSchema,
 } from 'src/schemas/auth/sign-in.schema';
-import { cn } from '@/lib/utils';
+import { ControlledInput, Form } from '@/components/app/form';
+import { SignInData } from '@/types';
 
 export default function SignIn() {
   const auth = useAuth();
 
   const [loading, setLoading] = useState(false);
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<signInFormData>({
-    resolver: zodResolver(signInFormSchema),
-    reValidateMode: 'onSubmit',
+  const form = useForm<signInFormData>({
     defaultValues: {
       email: '',
       password: '',
     },
+    reValidateMode: 'onSubmit',
+    resolver: zodResolver(signInFormSchema),
   });
 
-  const onSubmit = async (data: any) => {
+  const onSubmit = async (data: SignInData) => {
     if (loading) {
       return;
     }
@@ -50,57 +47,48 @@ export default function SignIn() {
 
   return (
     <View className="gap-3 px-4 flex-1 bg-white">
-      <View className="pt-1">
-        <Text className="text-base text-sm">Email</Text>
-        <Input
-          autoFocus
+      <Form form={form} onSubmit={onSubmit} className="gap-y-4">
+        <ControlledInput
+          name="email"
+          label="Email"
           placeholder="Digite seu email"
-          className={cn(errors.email && 'border-rose-400')}
-          inputMode="email"
-          {...register('email')}
+          autoCapitalize="none"
+          autoCorrect={false}
+          autoFocus
         />
-        {errors.email && (
-          <Text className="my-1 text-xs font-semibold text-destructive">
-            {errors.email.message}
-          </Text>
-        )}
-      </View>
-
-      <View>
-        <Text className="text-base text-sm">Senha</Text>
-        <Input
-          className={cn(errors.password && 'border-rose-400')}
+        <ControlledInput
+          name="password"
+          label="Senha"
           placeholder="Digite sua senha"
           secureTextEntry
-          {...register('password')}
         />
-        {errors.password && (
-          <Text className="my-1 text-xs font-semibold text-destructive">
-            {errors.password.message}
-          </Text>
-        )}
-      </View>
+      </Form>
+
       {/* TODO: enable password recovery */}
-      <View>
-        <Button size="lg" onPress={handleSubmit(onSubmit)} disabled={loading}>
+      <View className="gap-y-2">
+        <Button
+          size="lg"
+          onPress={form.handleSubmit(onSubmit)}
+          disabled={loading}
+        >
           {loading ? (
             <ActivityIndicator size="small" color="white" />
           ) : (
             <Text> Entrar </Text>
           )}
         </Button>
+        <Text className="text-center text-xs font-bold"> ou se preferir </Text>
+        <Button
+          size="default"
+          onPress={() => router.push('/profile-selector')}
+          disabled={loading}
+          variant="outline"
+        >
+          <Text className="text-foreground font-semibold text-md">
+            Cadastre-se
+          </Text>
+        </Button>
       </View>
-      <Text className="text-center text-xs font-bold"> ou se preferir </Text>
-      <Button
-        size="default"
-        onPress={() => router.push('/profile-selector')}
-        disabled={loading}
-        variant="outline"
-      >
-        <Text className="text-foreground font-semibold text-md">
-          Cadastre-se
-        </Text>
-      </Button>
     </View>
   );
 }
