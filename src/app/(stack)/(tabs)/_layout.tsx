b644b992a5faf01e9af35b86d1b49ef255b6dbe4
André Gabriel <andreon098@gmail.com>
2025-11-29 19:54:01 -0300

    feat: update home screen tabs

--- src/app/(stack)/(tabs)/_layout.tsx
+++ src/app/(stack)/(tabs)/_layout.tsx
@@ -1,25 +1,99 @@
-import { Ionicons, Octicons } from '@expo/vector-icons';
+import { Octicons } from '@expo/vector-icons';
 import { DrawerActions } from '@react-navigation/native';
+import { debounce } from 'es-toolkit/function';
 import { Image } from 'expo-image';
-import { Redirect, router, Tabs, useNavigation } from 'expo-router';
-import React from 'react';
-import { Pressable } from 'react-native';
+import {
+  Link,
+  Redirect,
+  router,
+  Tabs,
+  useNavigation,
+  useRouter,
+} from 'expo-router';
+import { CopyPlus, Globe, Star, UserRound } from 'lucide-react-native';
+import { useCallback } from 'react';
+import { Pressable, View } from 'react-native';
 import colors from 'src/constants/colors';
 import { PostProvider } from 'src/contexts/posts';
 import { SearchProvider } from 'src/contexts/search';
 import { useAuth } from 'src/hooks/useAuth';
+import { Button } from '@/components/ui/button';
+import { Icon } from '@/components/ui/icon';
+
+const TabIcon = ({
+  focused,
+  icon: IconComponent,
+  color,
+  size,
+  lib = 'lucide',
+  fill = true,
+}) => {
+  const iconContent =
+    lib === 'lucide' ? (
+      <Icon
+        as={IconComponent}
+        size={size}
+        color={color}
+        className={focused && fill ? 'fill-primary' : ''}
+      />
+    ) : (
+      <IconComponent name={(props) => props.name} size={size} color={color} />
+    );
+
+  if (!focused) {
+    return iconContent;
+  }
+
+  return (
+    <View
+      style={{
+        width: 3 * size,
+        height: size + 8,
+      }}
+      className="items-center justify-center bg-green-700/20 rounded-full"
+    >
+      {iconContent}
+    </View>
+  );
+};
+
+const HeaderAvatar = ({ user }) => {
+  const navigation = useNavigation();
+  return (
+    <Pressable
+      onPress={() => navigation.dispatch(DrawerActions.toggleDrawer())}
+    >
+      <Image
+        source={{ uri: user?.media?.remoteUrl }}
+        placeholder={{ blurhash: user?.media?.blurhash }}
+        className="h-9 w-9 rounded-full"
+      />
+    </Pressable>
+  );
+};
+
+const HeaderCreateButton = () => {
+  const router = useRouter();
+
+  const onPress = useCallback(
+    debounce(() => {
+      router.push('/post-create');
+    }, 170),
+    [router]
+  );
+  return (
+    <Button onPress={onPress} variant="ghost" className="p-2 rounded-full">
+      <Icon as={CopyPlus} size={24} className="stroke-text-neutral" />
+    </Button>
+  );
+};
 
 const TabLayout = () => {
-  const navigation = useNavigation();
   const { isDonor, token, user } = useAuth();
 
   if (!user || !token) {
     return <Redirect href="welcome" />;
   }
-
-  const onToggle = () => {
-    navigation.dispatch(DrawerActions.toggleDrawer());
-  };
 
   return (
     <PostProvider token={token}>
@@ -28,108 +102,86 @@ const TabLayout = () => {
           screenOptions={{
             tabBarShowLabel: false,
             tabBarActiveTintColor: colors.primary,
-            tabBarInactiveTintColor: colors.text_dark,
-            tabBarStyle: {
-              borderTopWidth: 1,
-              paddingTop: 4,
-            },
+            tabBarInactiveTintColor: 'rgb(46 56 77 / 0.8)',
+            tabBarStyle: { borderTopWidth: 1, paddingTop: 4 },
           }}
         >
           <Tabs.Screen
             name="index"
             options={{
               title: 'REAPP',
+              headerTitleAlign: 'center',
               headerTitleStyle: {
                 fontFamily: 'reapp_bold',
                 color: colors.primary,
                 fontSize: 20,
               },
-              headerRight: () =>
-                !isDonor ? (
-                  <Ionicons
-                    name="duplicate-outline"
-                    size={28}
-                    color={colors.text_neutral}
-                    onPress={() => {
-                      router.push({ pathname: 'post-create' });
-                    }}
-                  />
-                ) : null,
-              headerTitleAlign: 'center',
+              headerStyle: { shadowColor: '#000' },
               headerLeftContainerStyle: { paddingLeft: 16 },
               headerRightContainerStyle: { paddingRight: 16 },
-              headerStyle: {
-                shadowColor: '#000',
-              },
-              headerLeft: () => (
-                <Pressable onPress={onToggle}>
-                  <Image
-                    source={{ uri: user.media?.remoteUrl }}
-                    placeholder={{ blurhash: user.media?.blurhash }}
-                    className="h-9 w-9 rounded-full"
-                  />
-                </Pressable>
-              ),
-              tabBarIcon: ({ color, size }) => (
-                <Octicons name="home" size={size} color={color} />
-              ),
+              headerLeft: () => <HeaderAvatar user={user} />,
+              headerRight: () => (!isDonor ? <HeaderCreateButton /> : null),
+              tabBarIcon: ({ color, size, focused }) =>
+                focused ? (
+                  <View
+                    style={{ width: 3 * size, height: size + 8 }}
+                    className="items-center justify-center bg-green-700/20 rounded-full"
+                  >
+                    <Octicons name="home-fill" size={size} color={color} />
+                  </View>
+                ) : (
+                  <Octicons name="home" size={size} color={color} />
+                ),
             }}
           />
+
           <Tabs.Screen
             name="explore"
             options={{
               headerShown: true,
-              tabBarIcon: ({ size, color }) => (
-                <Octicons name="search" size={size} color={color} />
+              tabBarIcon: ({ size, color, focused }) => (
+                <TabIcon
+                  focused={focused}
+                  size={size}
+                  color={color}
+                  icon={Globe}
+                  fill={false}
+                />
               ),
             }}
           />
 
-          {isDonor ? (
-            <Tabs.Screen
-              name="favorites"
-              options={{
-                headerShown: true,
-                tabBarIcon: ({ size, color }) => (
-                  <Octicons name="heart" size={size} color={color} />
-                ),
-              }}
-            />
-          ) : (
-            <Tabs.Screen
-              name="favorites"
-              options={{
-                headerShown: false,
-                href: null,
-                tabBarIcon: ({ size, color }) => (
-                  <Octicons name="heart" size={size} color={color} />
-                ),
-              }}
-            />
-          )}
+          <Tabs.Screen
+            name="favorites"
+            options={{
+              href: !isDonor && null,
+              headerShown: isDonor,
+              tabBarIcon: ({ size, color, focused }) => (
+                <TabIcon
+                  focused={focused}
+                  size={size}
+                  color={color}
+                  icon={Star}
+                />
+              ),
+            }}
+          />
 
-          {isDonor ? (
-            <Tabs.Screen
-              name="profile"
-              options={{
-                href: null,
-                headerShown: false,
-                tabBarIcon: ({ size, color }) => (
-                  <Ionicons name="person-outline" size={size} color={color} />
-                ),
-              }}
-            />
-          ) : (
-            <Tabs.Screen
-              name="profile"
-              options={{
-                headerShown: false,
-                tabBarIcon: ({ size, color }) => (
-                  <Ionicons name="person-outline" size={size} color={color} />
-                ),
-              }}
-            />
-          )}
+          <Tabs.Screen
+            name="profile"
+            options={{
+              href: isDonor && null,
+              headerShown: false,
+              tabBarIcon: ({ size, color, focused }) => (
+                <TabIcon
+                  focused={focused}
+                  size={size}
+                  color={color}
+                  icon={UserRound}
+                />
+              ),
+            }}
+          />
         </Tabs>
       </SearchProvider>
     </PostProvider>
