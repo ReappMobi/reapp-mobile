56911ef24b3e6afe07e2ce3fc4b7957a9666cf4d
André Gabriel <andreon098@gmail.com>
2025-11-30 14:40:05 -0300

    feat: update layout for post routes

--- src/app/(stack)/(post)/_layout.tsx
+++ src/app/(stack)/(post)/_layout.tsx
@@ -1,13 +1,53 @@
-import { Stack } from 'expo-router';
-import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { router, Stack } from 'expo-router';
+import { Pressable } from 'react-native';
+import { Text } from '@/components/ui/text';
 
 const Layout = () => {
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <Stack>
+      <Stack.Screen
+        name="create-post"
+        options={{
+          headerShown: true,
+          headerTitle: () => (
+            <Text className="font-bold text-lg text-black">Nova postagem</Text>
+          ),
+          headerTitleAlign: 'center',
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <Pressable onPress={() => router.dismissAll()}>
+              <Ionicons name="close" size={28} color="black" />
+            </Pressable>
+          ),
+          headerRight: () => (
+            <Pressable>
+              <Ionicons
+                name="ellipsis-horizontal-circle-outline"
+                size={24}
+                color="#ccc"
+              />
+            </Pressable>
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="post-comments/[id]"
+        options={{
+          headerShown: true,
+          headerTitle: () => (
+            <Text className="font-bold text-lg text-black">Comentários</Text>
+          ),
+          headerTitleAlign: 'center',
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <Pressable onPress={() => router.dismissAll()}>
+              <Ionicons name="chevron-back" size={28} color="black" />
+            </Pressable>
+          ),
+        }}
+      />
+    </Stack>
   );
 };
 
