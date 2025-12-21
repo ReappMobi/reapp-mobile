4044f0fa07f09864f2e7f38267b0bcdb4e866655
André Gabriel <andreon098@gmail.com>
2025-12-21 19:12:42 -0300

    refactor: remove unused debounce utility function

--- src/utils/functools.ts
+++
@@ -1,7 +0,0 @@
-export const debounce = (fn: (...args: any[]) => any, delay: number) => {
-  let timeoutId: NodeJS.Timeout;
-  return function (...args: any[]) {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => fn(...args), delay);
-  };
-};
