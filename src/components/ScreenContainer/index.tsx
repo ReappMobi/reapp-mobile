07eba14e5b5f6eb1570fb738690aef2c451096aa
André Gabriel <andreon098@gmail.com>
2026-01-22 12:05:41 -0300

    feat: fix ScreenContainer

--- src/components/ScreenContainer/index.tsx
+++ src/components/ScreenContainer/index.tsx
@@ -1,12 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function ScreenContainer({ children }) {
   return (
-    <SafeAreaView className="flex-1 px-4 py-4 bg-white">
-      {children}
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    <SafeAreaView className="flex-1 bg-background p-4">{children}</SafeAreaView>
   );
 }
