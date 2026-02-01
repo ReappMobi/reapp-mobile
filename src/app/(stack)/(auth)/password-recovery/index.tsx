b5d9cb86c38d3f2b19bc00f9cc5a2949ac388b37
André Gabriel <andreon098@gmail.com>
2026-02-01 18:21:09 -0300

    feat: update recovery page

--- src/app/(stack)/(auth)/password-recovery/index.tsx
+++ src/app/(stack)/(auth)/password-recovery/index.tsx
@@ -1,10 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Link, router } from 'expo-router';
-import { MailCheck, MailX } from 'lucide-react-native';
+import MailCheck from 'lucide-react-native/dist/esm/icons/mail-check';
+import MailX from 'lucide-react-native/dist/esm/icons/mail-x';
 import { useForm } from 'react-hook-form';
 import { ActivityIndicator, View } from 'react-native';
 import { z } from 'zod';
 import { ScreenContainer } from '@/components';
+import { SafeKeyboardAvoidView } from '@/components/app/containers/keyboard-avoid-view';
 import { ControlledInput, Form } from '@/components/app/form';
 import { Button } from '@/components/ui/button';
 import {
@@ -49,9 +51,11 @@ export default function Page() {
         icon: MailX,
       });
     },
-    onSuccess(data) {
-      const { token } = data;
-      router.push(`password-recovery/code?=${token}`);
+    onSuccess({ token }, { email }) {
+      router.push({
+        pathname: 'password-recovery/enter-code',
+        params: { token, email },
+      });
       showToast({
         type: 'success',
         header: 'Email enviado',
@@ -73,48 +77,49 @@ export default function Page() {
   };
 
   return (
-    <ScreenContainer className="items-center justify-center">
-      <Card className="w-full max-w-sm">
-        <CardHeader className="flex-row mb-2">
-          <View className="flex-1 gap-1.5 items-center">
-            <CardTitle variant="h4">Esqueceu sua senha?</CardTitle>
-            <CardDescription variant="lead">
-              Digite seu email para recuperar sua senha
-            </CardDescription>
-          </View>
-        </CardHeader>
-        <CardContent>
-          <Form form={form} onSubmit={onSubmit}>
-            <View className="gap-y-5 items-center">
-              <ControlledInput name="email" placeholder="Digite seu email" />
-              <Button
-                size="lg"
-                onPress={form.handleSubmit(onSubmit)}
-                disabled={isPending}
-                className="w-full"
-              >
-                {isPending ? (
-                  <ActivityIndicator
-                    size="large"
-                    color={THEME['light'].primaryForeground}
-                  />
-                ) : (
-                  <Text> Enviar código de recuperação </Text>
-                )}
-              </Button>
-
-              <View className="flex-row items-center gap-x-1">
-                <Text variant="small">Lembrou sua senha?</Text>
-                <Link href="sign-in" asChild dismissTo>
-                  <Text variant="small" className="text-primary">
-                    Entrar
-                  </Text>
-                </Link>
-              </View>
+    <SafeKeyboardAvoidView>
+      <ScreenContainer className="items-center justify-center">
+        <Card className="w-full max-w-sm">
+          <CardHeader className="flex-row">
+            <View className="flex-1 gap-1.5 items-center">
+              <CardTitle variant="h4">Esqueceu sua senha?</CardTitle>
+              <CardDescription variant="lead">
+                Digite seu email para recuperar sua senha
+              </CardDescription>
             </View>
-          </Form>
-        </CardContent>
-      </Card>
-    </ScreenContainer>
+          </CardHeader>
+          <CardContent>
+            <Form form={form} onSubmit={onSubmit}>
+              <View className="gap-y-5 items-center">
+                <ControlledInput name="email" placeholder="Digite seu email" />
+                <Button
+                  size="lg"
+                  onPress={form.handleSubmit(onSubmit)}
+                  disabled={isPending}
+                  className="w-full"
+                >
+                  {isPending ? (
+                    <ActivityIndicator
+                      size="large"
+                      color={THEME['light'].primaryForeground}
+                    />
+                  ) : (
+                    <Text> Enviar código de recuperação </Text>
+                  )}
+                </Button>
+                <View className="flex-row items-center gap-x-1">
+                  <Text variant="small">Lembrou sua senha?</Text>
+                  <Link href="sign-in" asChild dismissTo>
+                    <Text variant="small" className="text-primary">
+                      Entrar
+                    </Text>
+                  </Link>
+                </View>
+              </View>
+            </Form>
+          </CardContent>
+        </Card>
+      </ScreenContainer>
+    </SafeKeyboardAvoidView>
   );
 }
