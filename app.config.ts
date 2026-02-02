103cfdb201207e1fe333717463208bf760b948bc
André Gabriel <andreon098@gmail.com>
2026-02-02 14:35:16 -0300

    chore: update version

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     name: isProduction ? 'Reapp' : 'Reapp (Dev)',
     slug: 'reapp',
     scheme: 'reapp',
-    version: '2.3.2',
+    version: '2.4.0',
     orientation: 'portrait',
     icon: './assets/icon.png',
     newArchEnabled: true,
