9172f58aab875b7adc08668e3d3d011d3a990670
André Gabriel <andreon098@gmail.com>
2026-02-02 10:20:18 -0300

    style: update card styles

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: ViewProps & React.RefAttributes<View>) {
     <TextClassContext.Provider value="text-card-foreground">
       <View
         className={cn(
-          'bg-card border-border flex flex-col gap-6 rounded-xl border py-6 shadow-sm shadow-black/5',
+          'bg-card border-border flex flex-col gap-6 rounded-xl border py-6 shadow-sm shadow-black/5 max-w-md w-full',
           className
         )}
         {...props}
