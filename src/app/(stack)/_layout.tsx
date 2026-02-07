a496bc3021a77945f8af3fd7d4d78c164a558d80
André Gabriel <andreon098@gmail.com>
2026-02-07 13:07:27 -0300

    feat: move search provider to main layout

--- src/app/(stack)/_layout.tsx
+++ src/app/(stack)/_layout.tsx
@@ -1,24 +1,27 @@
 import { Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SearchProvider } from '@/contexts/search.context';
 import { THEME } from '@/lib/theme';
 
 export default function Layout() {
   return (
     <SafeAreaProvider>
-      <Stack screenOptions={{ headerShown: false }} initialRouteName="(tabs)">
-        <Stack.Screen
-          name="institution"
-          options={{
-            headerShown: true,
-            headerShadowVisible: false,
-            headerTitle: '',
-            headerStyle: {
-              backgroundColor: THEME['light'].background,
-            },
-            headerTintColor: THEME['light'].foreground,
-          }}
-        />
-      </Stack>
+      <SearchProvider>
+        <Stack screenOptions={{ headerShown: false }} initialRouteName="(tabs)">
+          <Stack.Screen
+            name="institution"
+            options={{
+              headerShown: true,
+              headerShadowVisible: false,
+              headerTitle: '',
+              headerStyle: {
+                backgroundColor: THEME['light'].background,
+              },
+              headerTintColor: THEME['light'].foreground,
+            }}
+          />
+        </Stack>
+      </SearchProvider>
     </SafeAreaProvider>
   );
 }
