856eed82530c3b79ea0d066be8b718e7605cd28b
André Gabriel <andreon098@gmail.com>
2026-02-07 13:06:36 -0300

    feat: simplify  the _layout of explore

--- src/app/(stack)/(tabs)/explore/_layout.tsx
+++ src/app/(stack)/(tabs)/explore/_layout.tsx
@@ -1,34 +1,7 @@
-import { debounce } from 'es-toolkit/function';
-import { useNavigation } from 'expo-router';
-import { useEffect } from 'react';
-
 import { ScreenContainer } from 'src/components';
-import { useSearch } from 'src/hooks/useSearch';
-import { SearchInput } from '@/components/ui/input';
 import { MaterialTopTabs, TopTabs } from '@/components/ui/top-tab';
 
 const ExploreLayout = () => {
-  const { updateSearchQuery, updateSearchActive } = useSearch();
-  const navigation = useNavigation();
-
-  useEffect(() => {
-    navigation.setOptions({
-      headerTitle: () => (
-        <SearchInput
-          onChangeText={debounce(
-            (text: string) => updateSearchQuery(text),
-            300
-          )}
-          onFocus={() => updateSearchActive(true)}
-          onDimiss={() => {
-            updateSearchActive(false);
-            updateSearchQuery('');
-          }}
-        />
-      ),
-    });
-  }, []);
-
   return (
     <ScreenContainer className="p-0">
       <TopTabs>
